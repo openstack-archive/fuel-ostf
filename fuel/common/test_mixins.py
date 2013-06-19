dd6d03d78a754341f34a1f858cd97d8e3f9a2ab4
ekonstantinov <ekonstantinov@mirantis.com>
2013-06-19 20:06:39 +0300

    adding mixin class with assert methods that will add human readable messagies to the failed tests

---
+++ fuel/common/test_mixins.py
@@ -0,0 +1,71 @@
+# Copyright 2013 Mirantis, Inc.
+# All Rights Reserved.
+
+
+class FuelTestAssertMixin(object):
+    """
+
+    Mixin class with a set of assert methods created to abstract
+    from unittest assertion methods and provide human readable descriptions where it is possible
+    """
+    def verify_response_status(self, status, application='Application', msg=None):
+        """
+
+        Method provides human readable message for the HTTP response status verification
+
+        :param application: the name of application requested
+        :param status: response status
+        :param msg: message to be used instead the default one
+        """
+        if status == 200:
+            return
+
+        human_readable_statuses = {
+            400: 'Something changed in {application} and request is no longer recognized as valid. '
+                 'Please verify that you are not trying to address HTTP request to HTTPS socket',
+            401: 'Unauthorized, please check Keystone and {application connectivity}',
+            403: 'Forbidden, please verify Keystone and {application} security policies did not change',
+            404: '{application} server is running but application is not found',
+            500: '{application} server is experiencing some problems',
+            503: '{application} server is experiencing problems'
+        }
+
+        human_readable_status_groups = {
+            3: 'Status {status}. Redirection. Please check that all {application} proxy settings are set correctly',
+            4: 'Status {status}. Client error. Please verify that your {application} configurations corresponds '
+               'to re one defined in Fuel configuration ',
+            5: 'Status {status}. Server error. Please check {application} logs'
+        }
+
+        if status in human_readable_statuses:
+            status_msg = human_readable_statuses[status].\
+                            format(status=status, application=application)
+        else:
+            status_msg = human_readable_status_groups.get(status/100, '{application} status - {status} is unknown').\
+                            format(status=status, application=application)
+
+        self.assertEquals(200, status, ''.join(('Status - {status}'.format(status=status), status_msg, '\n', msg)))
+
+    def verify_response_body(self, body, content):
+        """
+
+        Method provides human readable message for the verification if
+        HTTP response body contains desired keyword
+
+        :param body: response body
+        :param content: content that should be present in the response body
+        """
+        if content in body:
+            return
+
+
+
+
+
+
+
+
+        self.asserTrue(content in body)
+
+
+
