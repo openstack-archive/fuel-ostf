78b2f99d9dbaea698d037dca880593f4e39ffb94
Sergey Lyopka <slyopka@mirantis.com>
2013-06-20 15:33:25 +0300

    OSTF-116-136: refactoring ssh client usage
      * SSH client moved to separated utility class ssh.py
      * test_sanity_infrastructure.py
    
    Change-Id: Ia0de1a363587c7cffb993f2d9f158a502a92cec1

--- fuel/tests/sanity/test_sanity_infrastructure.py
+++ fuel/tests/sanity/test_sanity_infrastructure.py
@@ -1,4 +1,4 @@
-import paramiko
+from fuel.common.ssh import Client as SSHClient
 from fuel.test import attr
 from fuel.tests.sanity import base
 
@@ -9,26 +9,12 @@ class SanityInfrastructureTest(base.BaseComputeAdminTest):
     @attr(type=['sanity', 'fuel'])
     def test_services_state(self):
         list_of_expected_services = self.config.compute.enabled_services
-        hostname = self.config.compute.controller_node
-        port = 22
-        username = self.config.compute.controller_node_ssh_user
-        password = self.config.compute.controller_node_ssh_password
-        sshClient = paramiko.SSHClient()
-        sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        try:
-            sshClient.connect(hostname, port, username, password)
-            _, out, err = sshClient.exec_command("nova-manage service list")
-            out = out.readlines()
-            err = err.readlines()
-        except BaseException:
-            raise
-        finally:
-            sshClient.close()
-        self.assertEqual("[]",
-                         str(err),
-                         ("nova-manage service list command has "
-                          "finished with the following error %s") % err)
-        self.assertFalse(u'XXX' in out)
+        host = self.config.compute.controller_node
+        usr = self.config.compute.controller_node_ssh_user
+        pwd = self.config.compute.controller_node_ssh_password
+        output = SSHClient(host, usr, pwd).exec_command(
+            "nova-manage service list")
+        self.assertFalse(u'XXX' in output)
         self.assertEqual(len(list_of_expected_services),
-                         str(out).count(u':-)'),
+                         output.count(u':-)'),
                          'Not all the expected services started')
