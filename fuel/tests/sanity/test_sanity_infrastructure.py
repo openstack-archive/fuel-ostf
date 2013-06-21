8ceb6f0085283b62db6d9a78953071b3b015906a
Sergey Lyopka <slyopka@mirantis.com>
2013-06-21 13:59:00 +0300

    Add test for DNS check OSTF-116-135
    Change-Id: I9e00e9cb25f8a026881a3b706709d83f650723d1

--- fuel/tests/sanity/test_sanity_infrastructure.py
+++ fuel/tests/sanity/test_sanity_infrastructure.py
@@ -6,15 +6,35 @@ from fuel.tests.sanity import base
 class SanityInfrastructureTest(base.BaseComputeAdminTest):
     _interface = 'json'
 
+    @classmethod
+    def setUpClass(cls):
+        cls.list_of_expected_services = cls.config.compute.enabled_services
+        cls.host = cls.config.compute.controller_node
+        cls.usr = cls.config.compute.controller_node_ssh_user
+        cls.pwd = cls.config.compute.controller_node_ssh_password
+        cls.hostname = cls.config.compute.controller_node_name
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
     @attr(type=['sanity', 'fuel'])
     def test_services_state(self):
-        list_of_expected_services = self.config.compute.enabled_services
-        host = self.config.compute.controller_node
-        usr = self.config.compute.controller_node_ssh_user
-        pwd = self.config.compute.controller_node_ssh_password
-        output = SSHClient(host, usr, pwd).exec_command(
+        output = SSHClient(self.host, self.usr, self.pwd).exec_command(
             "nova-manage service list")
         self.assertFalse(u'XXX' in output)
-        self.assertEqual(len(list_of_expected_services),
+        self.assertEqual(len(self.list_of_expected_services),
                          output.count(u':-)'),
                          'Not all the expected services started')
+
+    @attr(type=['sanity', 'fuel'])
+    def test_dns_state(self):
+        output = ''
+        try:
+            output = SSHClient(self.host, self.usr, self.pwd).exec_command(
+                "host " + self.host)
+        finally:
+            expected_output = 'in-addr.arpa domain name pointer ' + \
+                              self.hostname
+            self.assertTrue(expected_output in output,
+                            'DNS name cannot be resolved')
