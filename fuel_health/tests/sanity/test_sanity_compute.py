46df9369cd0b2a475d2113e83149abf9f7df0b0b
anaboikina <anaboikina@mirantis.com>
2013-07-12 10:37:03 +0300

    Added multi-line docstrings to tests.

--- fuel_health/tests/sanity/test_sanity_compute.py
+++ fuel_health/tests/sanity/test_sanity_compute.py
@@ -10,7 +10,13 @@ class SanityComputeTest(base.BaseComputeTest):
 
     @attr(type=['sanity', 'fuel'])
     def test_list_instances(self):
-        """Test checks that existing instances can be listed."""
+        """Test checks list of instances is available.
+        Target component: Nova
+        Scenario:
+            1. Request list of instances.
+            2. Check response status is equal to 200.
+            3. Check response contains "servers" section.
+        """
         resp, body = self.servers_client.list_servers()
         self.verify_response_status(resp.status, u'Nova')
         self.verify_response_body(body, u'servers',
@@ -19,7 +25,13 @@ class SanityComputeTest(base.BaseComputeTest):
 
     @attr(type=['sanity', 'fuel'])
     def test_list_images(self):
-        """Test checks that existing images can be listed."""
+        """Test checks list of images is available.
+        Target component: Glance
+
+        Scenario:
+            1. Request list of images.
+            2. Check response status is equal to 200.
+            3. Check response contains "images" section."""
         resp, body = self.images_client.list_images()
         self.verify_response_status(resp.status, 'Glance')
         self.verify_response_body(body, u'images',
@@ -28,7 +40,13 @@ class SanityComputeTest(base.BaseComputeTest):
 
     @attr(type=['sanity', 'fuel'])
     def test_list_volumes(self):
-        """Test checks that existing volumes can be listed."""
+        """Test checks list of volumes is available.
+        Target component: Swift
+
+        Scenario:
+            1. Request list of volumes.
+            2. Check response status is equal to 200.
+            3. Check response contains "volumes" section."""
         resp, body = self.volumes_client.list_volumes()
         self.verify_response_status(resp.status, 'Swift')
         self.verify_response_body(body, u'volumes',
@@ -37,7 +55,13 @@ class SanityComputeTest(base.BaseComputeTest):
 
     @attr(type=['sanity', 'fuel'])
     def test_list_snapshots(self):
-        """Test checks that existing snapshots can be listed."""
+        """Test checks list of snapshots is available.
+        Target component: Swift
+
+        Scenario:
+            1. Request list of snapshots.
+            2. Check response status is equal to 200.
+            3. Check response contains "snapshots" section.."""
         resp, body = self.snapshots_client.list_snapshots()
         self.verify_response_status(resp.status, 'Swift')
         self.verify_response_body(body, u'snapshots',
@@ -46,7 +70,13 @@ class SanityComputeTest(base.BaseComputeTest):
 
     @attr(type=['sanity', 'fuel'])
     def test_list_flavors(self):
-        """Test checks that existing flavors can be listed."""
+        """Test checks list of flavors is available.
+        Target component: Nova
+
+        Scenario:
+            1. Request list of flavors.
+            2. Check response status is equal to 200.
+            3. Check response contains "flavors" section."""
         resp, body = self.flavors_client.list_flavors()
         self.verify_response_status(resp.status, 'Nova')
         self.verify_response_body(body, u'flavors',
@@ -55,7 +85,13 @@ class SanityComputeTest(base.BaseComputeTest):
 
     @attr(type=['sanity', 'fuel'])
     def test_list_rate_limits(self):
-        """Test checks that absolute limits can be listed."""
+        """Test checks list of absolute limits is available.
+        Target component: Cinder
+
+        Scenario:
+            1. Request list of limits.
+            2. Check response status is equal to 200.
+            3. Check response contains absolute limits in "limits" section."""
         resp, body = self.limits_client.get_absolute_limits()
         self.verify_response_status(resp.status, 'Nova')
         self.verify_response_body(body["limits"], u'absolute',
