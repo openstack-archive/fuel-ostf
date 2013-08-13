38ed3d80062cb01f71e564b4767e1f0fecdf3640
Sergey Lyopka <slyopka@mirantis.com>
2013-08-13 17:27:41 +0300

    OSTF-211. Minor fix after test execution.

--- fuel_health/tests/smoke/test_nova_create_instance_with_connectivity.py
+++ fuel_health/tests/smoke/test_nova_create_instance_with_connectivity.py
@@ -348,8 +348,8 @@ class TestNovaNetwork(nmanager.NovaNetworkScenarioTest):
 
             self.servers.append(server)
         server = self.servers[-1]
-        instance_ip = server.__dict__['addresses']['novanetwork'][0]['addr']
-        compute = server.__dict__['OS-EXT-SRV-ATTR:host']
+        instance_ip = getattr(server, 'addresses')['novanetwork'][0]['addr']
+        compute = getattr(server, 'OS-EXT-SRV-ATTR:host')
 
         self.verify(100, self._check_connectivity_from_vm,
                         3, ("Connectivity to 8.8.8.8 from the VM doesn`t "
