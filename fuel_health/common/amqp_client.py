0f94ba3331ec54f76a9cb16e34e235140a1eefaf
Sergey Lyopka <slyopka@mirantis.com>
2013-08-20 17:49:13 +0300

    * test_003_rabbit_messages was added *  AMQP client class was refactored to provide utility methods    via HTTP RabbitMQ API

--- fuel_health/common/amqp_client.py
+++ fuel_health/common/amqp_client.py
@@ -1,5 +1,9 @@
+import time
+import logging
+
 from fuel_health.common.ssh import Client as SSHClient
 
+LOG = logging.getLogger(__name__)
 
 class RabbitClient(object):
     def __init__(self, host, username, password, key, timeout, rabbit_username, rabbit_password):
@@ -16,62 +20,102 @@ class RabbitClient(object):
                              key_filename=self.key_file,
                              timeout=self.timeout)
 
-    def _query(self, query, type='-XGET', arguments=''):
-        return 'curl -i -u {ruser}:{rpass} -H "content-type:application/json"'\
-                ' {type} {args} http://localhost:55672/api/{query}'.format(
-            ruser=self.rabbit_user,
-            rpass=self.rabbit_password,
-            type=type, query=query,
-            args=arguments)
-
     def list_queues(self):
-        query = self._query('/queues/')
-        return self.ssh.exec_command(query)
+        query = self._query('/queues?columns=name&sort=name')
+        return self._execute(query)
 
     def create_queue(self, queue_name):
         query = self._query(
-            query='/queues/%2f/{queue_name}'.format(queue_name=queue_name),
+            query='queues/%2f/{queue_name}'.format(queue_name=queue_name),
             type='-XPUT',
-            arguments='-d \{\}')
-        return self.ssh.exec_command(query)
+            arguments='-d \'{}\''
+        )
+        return self._execute(query)
 
-        # def send_message(self, queue_name, message):
-        #     connection = pika.BlockingConnection(
-        #         pika.ConnectionParameters(host=self.host,
-        #                                   credentials=self.credentials))
-        #     channel = connection.channel()
-        #     channel.queue_declare(queue=queue_name, passive=True)
-        #     channel.basic_publish(exchange='',
-        #                           routing_key=queue_name,
-        #                           body=message)
-        #     connection.close()
-        #
-        # def receive_message(self, queue_name):
-        #     connection = pika.BlockingConnection(
-        #         pika.ConnectionParameters(host=self.host,
-        #                                   credentials=self.credentials))
-        #     channel = connection.channel()
-        #     channel.queue_declare(queue=queue_name, passive=True)
-        #     method_frame, _, message = channel.basic_get(queue=queue_name)
-        #     if method_frame is None:
-        #         connection.close()
-        #         return None
-        #     else:
-        #         channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-        #         connection.close()
-        #         return message
-        #
-        # def empty_queue(self, queue_name):
-        #     message = 'Something'
-        #     while message:
-        #         message = self.receive_message(queue_name)
-        #
-        # def close(self, queue_name):
-        #     connection = pika.BlockingConnection(
-        #         pika.ConnectionParameters(host=self.host,
-        #                                   credentials=self.credentials))
-        #     channel = connection.channel()
-        #     try:
-        #         channel.queue_delete(queue=queue_name)
-        #     except pika.exceptions.AMQPChannelError:
-        #         pass
+    def delete_queue(self, queue_name):
+        query = self._query(
+            query='queues/%2f/{queue_name}'.format(queue_name=queue_name),
+            type='-XDELETE'
+        )
+        return self._execute(query)
+
+    def create_exchange(self, exchange_name):
+        query = self._query(
+            query='exchanges/%2f/{name}'.format(name=exchange_name),
+            type='-XPUT',
+            arguments='-d \'{"type":"direct"}\''
+        )
+        return self._execute(query)
+
+    def delete_exchange(self, exchange_name):
+        query = self._query(
+            query='exchanges/%2f/{name}'.format(name=exchange_name),
+            type='-XDELETE'
+        )
+        return self._execute(query)
+
+    def create_binding(self, exchange_name, queue_name, binding_name):
+        query = self._query(
+            query='bindings/%2f/e/{ename}/q/{qname}/{name}'.format(
+                ename=exchange_name,
+                qname=queue_name,
+                name=binding_name
+            ),
+            type='-XPUT'
+        )
+        return self._execute(query)
+
+    def delete_binding(self, exchange_name, queue_name, binding_name):
+        query = self._query(
+            query='bindings/%2f/e/{ename}/q/{qname}/{name}'.format(
+                ename=exchange_name,
+                qname=queue_name,
+                name=binding_name
+            ),
+            type='-XDELETE'
+        )
+        return self._execute(query)
+
+    def publish_message(self, message, exchange, binding):
+        query = self._query(
+            query='exchanges/%2f/{ename}/publish'.format(ename=exchange),
+            type='-XPOST',
+            arguments='-d \'{{"properties":{{}},"routing_key":"{bname}",'
+                      '"payload":"{msg}","payload_encoding":"string"}}\''.format(
+                bname=binding,
+                msg=message
+            )
+        )
+        return self._execute(query)
+
+    def get_message(self, queue):
+        query = self._query(
+            query='queues/%2f/{qname}/get'.format(qname=queue),
+            type='-XPOST',
+            arguments='-d \'{"count":1,"requeue":false,"encoding":"auto"}\''
+        )
+        return self._execute(query)
+
+    def _query(self, query, header=True, type='-XGET', arguments=''):
+        start = (header and 'curl -i') or 'curl'
+        return '{start} -u {ruser}:{rpass} -H "content-type:application/json"'\
+                ' {type} {args} http://localhost:55672/api/{query}'.format(
+            start=start,
+            ruser=self.rabbit_user,
+            rpass=self.rabbit_password,
+            type=type, query=query,
+            args=arguments
+        )
+
+    def _execute(self, query, times=5):
+        exception = None
+        for i in range(times+1):
+            try:
+                result = self.ssh.exec_command(query)
+                if result:
+                    return result
+                time.sleep(1)
+            except Exception as exc:
+                exception = exc
+                LOG.debug(exc)
+        raise exception
--- fuel_health/tests/ha/test_rabbit.py
+++ fuel_health/tests/ha/test_rabbit.py
@@ -5,6 +5,7 @@ from nose.plugins.attrib import attr
 from fuel_health import config
 from fuel_health.common.amqp_client import RabbitClient
 from fuel_health.common.ssh import Client as SSHClient
+from fuel_health.common.utils.data_utils import rand_name
 from fuel_health.test import BaseTestCase
 
 LOG = logging.getLogger(__name__)
@@ -34,6 +35,8 @@ class RabbitSmokeTest(BaseTestCase):
         cls._pwd = cls.config.compute.controller_node_ssh_password
         cls._key = cls.config.compute.path_to_private_key
         cls._ssh_timeout = cls.config.compute.ssh_timeout
+        cls._rabbit_user = 'nova'
+        cls._rabbit_password = 'D4ZfVmsF'
         cls.amqp_clients = [RabbitClient(cnt,
                                          cls._usr,
                                          cls._pwd, cls._key,
@@ -43,7 +46,7 @@ class RabbitSmokeTest(BaseTestCase):
                             for cnt in cls._controllers]
 
     @attr(type=['fuel', 'ha', 'non-destructive'])
-    def test_rabbitmqctl_status(self):
+    def test_001_rabbitmqctl_status(self):
         """RabbitMQ cluster availability
 
         Scenario:
@@ -91,12 +94,12 @@ class RabbitSmokeTest(BaseTestCase):
                                                       node["controller"]))
 
     @attr(type=['fuel', 'ha', 'non-destructive'])
-    def test_rabbit_queues(self):
+    def test_002_rabbit_queues(self):
         """RabbitMQ queues availability
         Scenario:
           1. Retrieve list of RabbitMQ queues for each controller
           2. Check the same queue list is present on each node
-        Duration: 100
+        Duration: 100 s.
         """
         if not self._controllers:
             self.fail('Step 1 failed: There are no controller nodes.')
@@ -118,29 +121,93 @@ class RabbitSmokeTest(BaseTestCase):
                 #this means it is the first node,
                 #this case we check there are queues only
                 temp_set = output
-                self.assertTrue(output, "Step 2 failed: Queue list for %s"
-                                        " controller is empty" % node)
+                if not output:
+                    self.fail("Step 2 failed: Queue list for %s"
+                              " controller is empty" % node)
                 continue
                 #check all the queues are present on all the nodes
-            self.assertFalse(output.symmetric_difference(temp_set),
-                             "Step 2 failed: Queue lists are different for %s "
-                             "and %s controllers" %
-                             (self._controllers[0], node))
+            if output.symmetric_difference(temp_set):
+                self.fail("Step 2 failed: Queue lists are different for %s "
+                          "and %s controllers" %
+                          (self._controllers[0], node))
 
     @attr(type=['fuel', 'ha', 'non-destructive'])
-    def test_rabbit_messages(self):
+    def test_003_rabbit_messages(self):
         """RabbitMQ messages availability
         Scenario:
-          1. Create a queue on one of the controllers
-          2. Check the same queue is available on each the controller
-          3. Publish message to the queue.
-          4. Check the message is available on other controllers
-        Duration: 100
+          1. Create a queue on a controller
+          2. Create an exchange on the controller
+          3. Create a binding for the queue and exchange
+          4. Publish messages to the queue.
+          5. Check the messages are available on other controllers
+          6. Delete the exchange
+          7. Delete the queue (with binding)
+        Duration: 100 s.
         """
         if not self._controllers:
             self.fail('Step 1 failed: There are no controller nodes.')
 
-        self.fail(self.amqp_clients[0].list_queues())
+        new_queue = rand_name(name='ostf1-test-queue-')
+        new_exchange = rand_name(name='ostf1-test-exchange-')
+        new_binding = rand_name(name='ostf1-test-binding-')
+        first_client = self.amqp_clients[0]
+        result = self.verify(20, first_client.create_queue, 1,
+                    "Cannot create queue {name}.".format(name=new_queue),
+                    "Queue creation.", new_queue)
+        self.verify_response_true('204 No Content' in result,
+                                  'Step 1 failed: {queue} queue cannot be '
+                                  'created on {ctlr} controller.'.format(
+                                      queue=new_queue, ctlr=first_client.host))
+
+        result = self.verify(20, first_client.create_exchange, 2,
+                    "Cannot create exchange {name}.".format(name=new_exchange),
+                    "Exchange creation.", new_exchange)
+        self.verify_response_true('204 No Content' in result,
+                                  'Step 2 failed: {ex} exchange cannot be '
+                                  'created on {ctlr} controller.'.format(
+                                      ex=new_exchange, ctlr=first_client.host))
+
+        result = self.verify(20, first_client.create_binding, 3,
+                    "Cannot create binding {name}.".format(name=new_binding),
+                    "Binding creation.", new_exchange, new_queue, new_binding)
+        self.verify_response_true('204 No Content' in result,
+                                  'Step 2 failed: {bin} binding cannot be '
+                                  'created for {queue} queue and {ex} '
+                                  'exchange on {ctlr} controller.'.format(
+                                      ex=new_exchange, ctlr=first_client.host,
+                                      bin=new_binding, queue=new_queue))
+
+        client_id = 0
+        for clr in self._controllers:
+            result = self.verify(20, first_client.publish_message, 4,
+                        "Cannot push message.", "Message pushing.",
+                        "Test Message", new_exchange, new_binding)
+            self.verify_response_true('200 OK' in result,
+                                  'Step 4 failed: Message cannot be pushed.')
+            self.verify(20, self.amqp_clients[client_id].get_message, 5,
+                        "Cannot get message.", "Message receiving.",
+                        new_queue)
+            self.verify_response_true('200 OK' in result,
+                                  'Step 5 failed: Message cannot be received '
+                                  'on %s controller.' %
+                                  self.amqp_clients[client_id].host)
+            client_id = client_id + 1
+
+        result = self.verify(20, first_client.delete_exchange, 6,
+                    "Cannot delete exchange {name}.".format(name=new_exchange),
+                    "Queue deletion.", new_exchange)
+        self.verify_response_true('204 No Content' in result,
+                                  'Step 6 failed: {ex} exchange cannot be '
+                                  'removed on {ctlr} controller.'.format(
+                                      ctlr=first_client.host, ex=new_exchange))
+
+        result = self.verify(20, first_client.delete_queue, 7,
+                    "Cannot delete queue {name}.".format(name=new_queue),
+                    "Queue deletion.", new_queue)
+        self.verify_response_true('204 No Content' in result,
+                                  'Step 7 failed: {queue} queue cannot be '
+                                  'removed on {ctlr} controller.'.format(
+                                      ctlr=first_client.host, queue=new_queue))
 
     def _format_output(self, output):
         """
