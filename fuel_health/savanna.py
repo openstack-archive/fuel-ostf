c41766c1f017cea18df93f9dda6b781b4f714761
Sergey Galkin <sgalkin@mirantis.com>
2013-09-06 17:35:18 +0400

    Added import

--- fuel_health/savanna.py
+++ fuel_health/savanna.py
@@ -14,3 +14,32 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import logging
+
+import contextlib
+import shutil
+import socket
+import telnetlib
+import time
+import json
+import os
+
+from fuel_health.common.ssh import Client as SSHClient
+from fuel_health.exceptions import SSHExecCommandFailed
+from fuel_health import exceptions
+import fuel_health.nmanager
+import fuel_health.test
+from fuel_health import config
+
+import keystoneclient.v2_0.client
+
+LOG = logging.getLogger(__name__)
+
+
+#class SavannaClient(fuel_health.manager.Manager):
+#    """
+#    Manager that provides access to the official python clients for
+#    calling various Savanna APIs.
+#    """
+#    def __init__(self):
+
