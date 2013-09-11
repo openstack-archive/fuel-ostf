8ea6fc57b299c6e2b5dd5b0ed59791c314250039
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2013-09-11 21:54:53 +0400

    Added sanity automated tests for Murano

---
+++ fuel_health/murano.py
@@ -0,0 +1,268 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 Mirantis, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from muranoclient.v1.client import Client
+import nmanager
+
+
+class MuranoTest(nmanager.OfficialClientTest):
+    """
+    Manager that provides access to the Murano python client for
+    calling Murano API.
+    """
+
+    client = None
+    insecure = None
+    api_host = None
+
+    def setUp(self):
+        """
+            This method allows to initialize authentication before
+            each test case and define parameters of Murano API Service
+            This method also create environment for all tests
+        """
+
+        super(MuranoTest, self).setUp()
+
+        " Get xAuth token from Keystone "
+        self.token_id = self.manager._get_identity_client(
+            self.config.identity.admin_username,
+            self.config.identity.admin_password,
+            self.config.identity.admin_tenant_name).auth_token
+
+        " Get Murano API parameters "
+        self.api_host = 'http://172.18.79.82:8082'#self.config.compute.murano_api_url
+        self.insecure = False  #self.config.compute.murano_insecure
+        
+        self.murano_client = self._get_murano_client()
+
+        self.environment = self.create_environment("Murano-OSTF-test_env01")
+        if not hasattr(self.environment, 'id'):
+            step='1. Send request to create environment. '
+            msg = ('Step %s failed: ' % step) + 'Can not create environment.'
+
+    def tearDown(self):
+        """
+            This method alows to clean up after each test.
+            The main task for this method - delete environment after
+            PASSED and FAILED tests.
+        """
+
+        result = self.delete_environment(self.environment.id)
+        if result:
+            msg = ' Can not delete environment. '
+            msg = ('Step %s failed: ' % str(self.last_step)) + msg
+            self.fail(msg)
+
+    def _get_murano_client(self):
+        """
+            This method returns Murano API client
+        """
+        return Client(endpoint=self.api_host,
+                      token=self.token_id,
+                      insecure=self.insecure)
+
+    def verify_elements_list(self, elements, attrs, msg='', failed_step=''):
+        """
+        Method provides human readable message for the verification of
+        list of elements with specific parameters
+        :param elements: the list of elements from response
+        :param attrs: required attributes for each element
+        :param msg: message to be used instead the default one
+        :param failed_step: step with failed action
+        """
+        if failed_step:
+            msg = ('Step %s failed: ' % str(failed_step)) + msg
+
+        if len(elements) == 0:
+            self.fail(msg)
+
+        for element in elements:
+            for attribute in attrs:
+                if not hasattr(element, attribute):
+                    self.fail(msg)
+
+    def list_environments(self):
+        """
+            This method allows to get the list of environments.
+
+            Returns the list of environments.
+        """
+
+        return self.murano_client.environments.list()
+
+    def create_environment(self, name):
+        """
+            This method allows to create environment.
+
+            Input parameters:
+              name - Name of new environment
+
+            Returns new environment.
+        """
+
+        return self.murano_client.environments.create(name)
+
+    def get_environment(self, environment_id, session_id=None):
+        """
+            This method allows to get specific environment by ID.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment (optional)
+
+            Returns specific environment.
+        """
+
+        return self.murano_client.environments.get(environment_id, session_id)
+
+    def update_environment(self, environment_id, new_name):
+        """
+            This method allows to update specific environment by ID.
+
+            Input parameters:
+              environment_id - ID of environment
+              new_name - New name for environment
+
+            Returns new environment.
+        """
+
+        return self.murano_client.environments.update(environment_id, new_name)
+
+    def delete_environment(self, environment_id):
+        """
+            This method allows to delete specific environment by ID.
+
+            Input parameters:
+              environment_id - ID of environment
+
+            Returns None.
+        """
+
+        return self.murano_client.environments.delete(environment_id)
+
+    def create_session(self, environment_id):
+        """
+            This method allows to create session for environment.
+
+            Input parameters:
+              environment_id - ID of environment
+
+            Returns new session.
+        """
+
+        return self.murano_client.sessions.configure(environment_id)
+
+    def get_session(self, environment_id, session_id):
+        """
+            This method allows to get specific session.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment
+
+            Returns specific session.
+        """
+
+        return self.murano_client.sessions.get(environment_id, session_id)
+
+    def delete_session(self, environment_id, session_id):
+        """
+            This method allows to delete session for environment.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment
+
+            Returns None.
+        """
+
+        return self.murano_client.sessions.delete(environment_id, session_id)
+
+    def deploy_session(self, environment_id, session_id):
+        """
+            This method allows to deploy session for environment.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment
+
+            Returns specific session.
+        """
+
+        return self.murano_client.sessions.deploy(environment_id, session_id)
+
+    def create_service(self, environment_id, session_id, json_data):
+        """
+            This method allows to create service.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment
+              json_data - JSON with service description
+
+            Returns specific service.
+        """
+
+        return self.murano_client.services.post(environment_id, path='/',
+                                                data=json_data, session_id=session_id)
+
+    def list_services(self, environment_id, session_id=None):
+        """
+            This method allows to get list of services.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment (optional)
+
+            Returns list of services.
+        """
+
+        return self.murano_client.services.get(environment_id, '/', session_id)
+
+    def get_service(self, environment_id, session_id, service_id):
+        """
+            This method allows to get service by ID.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment
+              service_id - ID of service in this environment
+
+            Returns specific service.
+        """
+
+        url = '/' + str(service_id)
+
+        return self.murano_client.services.get(environment_id, url, session_id)
+
+    def delete_service(self, environment_id, session_id, service_id):
+        """
+            This method allows to delete specific service.
+
+            Input parameters:
+              environment_id - ID of environment
+              session_id - ID of session for this environment
+              service_id - ID of service in this environment
+
+            Returns None.
+        """
+
+        url = '/' + str(service_id)
+
+        return self.murano_client.services.delete(environment_id, url,
+                                                  session_id)
---
+++ fuel_health/tests/sanity/test_sanity_murano.py
@@ -0,0 +1,89 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import murano
+
+class ServicesTestJSON(murano.MuranoTest):
+    """
+    TestClass contains tests that check basic Murano functionality.
+    """
+
+    def test_list_environments(self):
+        """List Of Environments
+        Test checks that user can get list of environments.
+        Target component: Murano
+
+        Scenario:
+            1. Send request to create environment.
+            2. Request the list of environments.
+            3. Clean up (delete environment).
+        Duration: 5 s.
+        """
+
+        fail_msg = 'User can not get list of environments.'
+        environments = self.verify(5, self.list_environments,
+                                   2, fail_msg, "environments listing")
+
+        step = '2. Request the list of environments.'
+        self.verify_elements_list(environments, ['id', 'name'],
+                                  msg=fail_msg, failed_step=step)
+
+        " Save info for tear down method "
+        self.last_step = '3. Clean up (delete environment).'
+
+    def test_create_and_delete_service(self):
+        """Create And Delete Service
+        Test checks that user can create service.
+        Target component: Murano
+
+        Scenario:
+            1. Send request to create environment.
+            2. Send request to create session for environment.
+            3. Send request to create service.
+            4. Request the list of services.
+            5. Send request to delete service.
+            6. Clean up (delete environment).
+        Duration: 20 s.
+        """
+
+        fail_msg = 'User can not create session for environment.'
+        session = self.verify(5, self.create_session,
+                              2, fail_msg, "session creating",
+                              self.environment.id)
+
+        srv = {"name": "new_service", "type": "test", "units": [{},]}
+        fail_msg = 'User can not create service.'
+        service = self.verify(5, self.create_service,
+                              3, fail_msg, "service creating",
+                              self.environment.id, session.id, srv)
+
+        step = '4. Request the list of services.'
+        fail_msg = 'User can not get list of services.'
+        services = self.verify(5, self.list_services,
+                               4, fail_msg, "services listing",
+                               self.environment.id, session.id)
+
+        self.verify_elements_list(services, ['id', 'name'],
+                                  msg='User can not get list of services',
+                                  failed_step=step)
+
+        fail_msg = 'User can not delete service.'
+        response = self.verify(5, self.delete_service,
+                               5, fail_msg, "service deleting",
+                               self.environment.id, session.id, service.id)
+
+        " Save info for tear down method "
+        self.last_step = '6. Clean up (delete environment).'
