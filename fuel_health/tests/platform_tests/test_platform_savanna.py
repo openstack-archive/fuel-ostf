a701f5564683fe28674869365c4e8d9a2d05ed31
Sergey Galkin <sgalkin@mirantis.com>
2013-10-24 15:04:38 +0400

    Fixed url for Savanna platform tests
    Change-Id: I07d3aaec499b93e645f968ff12dc57920005da8c

--- fuel_health/tests/platform_tests/test_platform_savanna.py
+++ fuel_health/tests/platform_tests/test_platform_savanna.py
@@ -46,10 +46,10 @@ class PlatformSavannaTests(savanna.SavannaTest):
 
         Deployment tags: Savanna
         """
-        fail_msg = "Savanna image with tags wasn't imported into Glance, " \
-                   "please check " \
-                   "http://docs.mirantis.com/fuel/fuel-3.2/installation" \
-                   "-fuel-ui.html#post-deployment-check. "
+        fail_msg = ("Savanna image with tags wasn't imported into Glance, "
+                    "please check "
+                    "http://docs.mirantis.com/fuel/fuel-3.2/user-guide.html"
+                    "#platform-tests-description")
         self.verify_response_true(
             self.verify(
                 20,
