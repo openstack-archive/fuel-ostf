c0f871f1065f2671204b9fcdcd90c42ccb8a2e48
Artem Roma <aroma@mirantis.com>
2013-10-25 15:48:01 +0300

    Fix alternative tags processing.
    One can now set on testsets (in profile attribute) and tests (in
    docstring) alternative deployment tags in form 'tag_one | tag_two'.
    Nose discovery plugin was modified in order to support new logic. New
    utility function process_deployment_tags was added to nose_utils.py file.
    This function now incapsulates all logic processing deployments tags in
    means that it provides logical test whether particular testset and its
    tests must be discovered for current cluster or not.
    
    Change-Id: I10199a1f20d6edfbe25f1be24e4d5662101d1b99

---
+++ fuel_plugin/tests/functional/dummy_tests/deployment_types_tests/alternative_depl_tags_test.py
@@ -0,0 +1,36 @@
+#    Copyright 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+__profile__ = {
+    "id": "alternative_depl_tags_test",
+    "driver": "nose",
+    "test_path": ("fuel_plugin/tests/functional/dummy_tests/"
+                  "deployment_types_tests/alternative_depl_tags_test.py"),
+    "description": "Fake testset to test alternative deployment tags",
+    "deployment_tags": ["alternative | alternative_test"]
+}
+
+import unittest
+
+
+class AlternativeDeplTagsTests(unittest.TestCase):
+
+    def test_simple_fake_test(self):
+        """fake empty test
+        This is fake empty test with
+        example of description of alternative
+        deployment tags
+        Deployment tags: one_tag| another_tag, other_tag
+        """
+        self.assertTrue(True)
