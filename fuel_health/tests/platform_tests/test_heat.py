e866a0e84829bf4bd395802b32628cc1976431ea
Yuriy Yekovenko <yyekovenko@mirantis.com>
2014-01-20 09:28:29 +0200

    Timeout increased for Heat autoscaling test.
    Timeout for cloud initialization increased to 1000 second.
    
    Change-Id: I83aee1bb69aa775838902fade3160424a717d4cc
    Closes-Bug: #1270172

--- fuel_health/tests/platform_tests/test_heat.py
+++ fuel_health/tests/platform_tests/test_heat.py
@@ -226,7 +226,7 @@ class HeatSmokeTests(heatmanager.HeatBaseTest):
             15. Delete the file with private key.
             16. Delete the stack.
             17. Wait for the stack to be deleted.
-        Duration: 2200 s.
+        Duration: 2600 s.
         """
         image_name = "F17-x86_64-cfntools"
         fail_msg = ("Image with cfntools package wasn't "
@@ -308,7 +308,7 @@ class HeatSmokeTests(heatmanager.HeatBaseTest):
         vm_connection = "ssh -o StrictHostKeyChecking=no -i %s %s@%s" % (
             path_to_key, "ec2-user", floating_ip.ip)
 
-        self.verify(600, self._wait_for_cloudinit, 10,
+        self.verify(1000, self._wait_for_cloudinit, 10,
                     "Cloud-init script cannot finish within timeout.",
                     "cloud-init script execution on VM",
                     vm_connection, 600, 15)
