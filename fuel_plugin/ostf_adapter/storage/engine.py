9c85406e84711fa59f2dee791c3bf2746331477f
Artem Roma <aroma@mirantis.com>
2014-02-24 15:49:38 +0200

    DB toolkit refactoring
    Code that controls db related toolkit (creating orm sessions and
    engines) has been refactored.
    This change was made with purpose of simplifiying and unifying of mentioned
    logic so that it can be reused by several processes withoud beind compromised
    by dependency on pecan.conf
    attribute.
    
    Change-Id: Ia62871f0d91a96d31aea7a5d1f1454a64d864f9a

--- fuel_plugin/ostf_adapter/storage/engine.py
+++ fuel_plugin/ostf_adapter/storage/engine.py
@@ -12,48 +12,71 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from pecan import conf
 from sqlalchemy import create_engine, orm, pool
 
 
 _ENGINE = None
 _MAKER = None
-_REDISINST = None
 
 
-def get_session(autocommit=True, expire_on_commit=False):
-    """Return a SQLAlchemy session."""
-    global _MAKER
-    global _SLAVE_MAKER
-    maker = _MAKER
+def instantiate_db_toolkit(dbpath, session_params):
+    '''Performs creation of SQLAlchemy engine and
+    Sessionmaker which will be saved in global variables
+    for further using.
 
-    if maker is None:
-        engine = get_engine()
-        maker = get_maker(engine, autocommit, expire_on_commit)
-
-    else:
-        _MAKER = maker
-
-    session = maker()
-    return session
-
-
-def get_engine(dbpath=None, pool_type=None):
-    """Return a SQLAlchemy engine."""
+    Params:
+        1) dbpath -- connection string for create_engine function
+        2) session_params -- dict with different params for settuping
+        sqlaclhemy session.
+    '''
     global _ENGINE
-    engine = _ENGINE
+    global _MAKER
 
-    if engine is None:
-        dbpath = dbpath if dbpath is not None else conf.dbpath
-        engine = create_engine(dbpath,
-                               poolclass=pool_type or pool.NullPool)
-    _ENGINE = engine
-    return engine
+    _ENGINE = create_engine(dbpath, poolclass=pool.NullPool)
+    _MAKER = orm.sessionmaker(bind=_ENGINE, **session_params)
 
 
-def get_maker(engine, autocommit=True, expire_on_commit=False):
-    """Return a SQLAlchemy sessionmaker using the given engine."""
-    return orm.sessionmaker(
-        bind=engine,
-        autocommit=autocommit,
-        expire_on_commit=expire_on_commit)
+def get_session():
+    """Returns a SQLAlchemy session."""
+    return _MAKER()
+
+
+def get_engine():
+    """Returns instance of sqlalchemy engine"""
+    return _ENGINE
+
+#def get_session(autocommit=True, expire_on_commit=False):
+#    """Return a SQLAlchemy session."""
+#    global _MAKER
+#    global _SLAVE_MAKER
+#    maker = _MAKER
+#
+#    if maker is None:
+#        engine = get_engine()
+#        maker = get_maker(engine, autocommit, expire_on_commit)
+#
+#    else:
+#        _MAKER = maker
+#
+#    session = maker()
+#    return session
+#
+#
+#def get_engine(dbpath=None, pool_type=None):
+#    """Return a SQLAlchemy engine."""
+#    global _ENGINE
+#    engine = _ENGINE
+#
+#    if engine is None:
+#        engine = create_engine(dbpath,
+#                               poolclass=pool_type or pool.NullPool)
+#    _ENGINE = engine
+#    return engine
+#
+#
+#def get_maker(engine, autocommit=True, expire_on_commit=False):
+#    """Return a SQLAlchemy sessionmaker using the given engine."""
+#    return orm.sessionmaker(
+#        bind=engine,
+#        autocommit=autocommit,
+#        expire_on_commit=expire_on_commit)
