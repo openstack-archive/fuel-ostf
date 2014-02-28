d04af5ea5514ba9dffe6a0215ad221311121f370
TimurNurlygayanov <tnurlygayanov@mirantis.com>
2014-02-28 14:13:37 +0400

    Fixed issue with parallel tests executions
    Change-Id: I8ae727ed5c242be29e7c3a60c870eedca4ce4b38
    Closes-Bug: #1285562

--- fuel_health/tests/platform_tests/test_murano.py
+++ fuel_health/tests/platform_tests/test_murano.py
@@ -62,7 +62,7 @@ class MuranoDeploymentSmokeTests(murano.MuranoTest):
         fail_msg = "Can't create environment. Murano API is not available. "
         self.environment = self.verify(15, self.create_environment,
                                        1, fail_msg, 'creating environment',
-                                       "ost1_test-Murano_env01")
+                                       self.env_name)
 
         fail_msg = "User can't create session for environment. "
         session = self.verify(5, self.create_session,
@@ -127,7 +127,7 @@ class MuranoDeploymentSmokeTests(murano.MuranoTest):
         fail_msg = "Can't create environment. Murano API is not available. "
         self.environment = self.verify(15, self.create_environment,
                                        1, fail_msg, 'creating environment',
-                                       "ost1_test-Murano_env01")
+                                       self.env_name)
 
         fail_msg = "User can't create session for environment. "
         session = self.verify(5, self.create_session,
@@ -195,7 +195,7 @@ class MuranoDeploymentSmokeTests(murano.MuranoTest):
         fail_msg = "Can't create environment. Murano API is not available. "
         self.environment = self.verify(15, self.create_environment,
                                        1, fail_msg, 'creating environment',
-                                       "ost1_test-Murano_env01")
+                                       self.env_name)
 
         fail_msg = "User can't create session for environment. "
         session = self.verify(5, self.create_session,
@@ -260,10 +260,11 @@ class MuranoDeploymentSmokeTests(murano.MuranoTest):
         Deployment tags: Murano, Heat
         """
 
+        env_name = rand_name(namestart="ost1_test-Murano_env")
         fail_msg = "Can't create environment. Murano API is not available. "
         self.environment = self.verify(15, self.create_environment,
                                        1, fail_msg, 'creating environment',
-                                       "ost1_test-Murano_env01")
+                                       self.env_name)
 
         fail_msg = "User can't create session for environment. "
         session = self.verify(5, self.create_session,
@@ -330,10 +331,11 @@ class MuranoDeploymentSmokeTests(murano.MuranoTest):
         Deployment tags: Murano, Heat
         """
 
+        env_name = rand_name(namestart="ost1_test-Murano_env")
         fail_msg = "Can't create environment. Murano API is not available. "
         self.environment = self.verify(15, self.create_environment,
                                        1, fail_msg, 'creating environment',
-                                       "ost1_test-Murano_env01")
+                                       self.env_name)
 
         fail_msg = "User can't create session for environment. "
         session = self.verify(5, self.create_session,
