5b5d2b7f9d895dbab0b5821961db89d5900de19b
Artem Roma <aroma@mirantis.com>
2014-03-20 17:23:01 +0200

    Added testrun orchestration
    Orchestration agent has been added to project. Main features:
     * orchestrator is implemented using amqp feature for communication, it
       makes logic of testrun management more flexible and extensible.
     * proxy logic - NoseDriver object has been replaced by several
       callbacks for the sake of simplicity.
     * orchestrator is started now as separate subprocess of ostf-server and
       is terminating on former's exit.
    
    Not implemented yet:
     * restart/stop actions management
     * unit and integration tests
    
    Change-Id: I92c5c86f9681c126ebe4560c42bbd1b41b350216

---
+++ fuel_plugin/ostf_adapter/orchestrator/producers.py
@@ -0,0 +1,27 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from kombu import Producer, Connection
+from base_stuff import default_conn_string, ostf_exchange
+
+LOG = logging.getLogger(__name__)
+
+
+def send_message(payload):
+    LOG.info('Senging message with payload: {0}'.format(payload))
+    with Connection(default_conn_string) as conn:
+        producer = Producer(conn, exchange=ostf_exchange)
+        producer.publish(payload, serializer='json')
