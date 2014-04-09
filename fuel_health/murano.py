56802c5402f003f5000e5de7ad3b8e1dd9569d38
Sergey Murashov <smurashov@mirantis.com>
2014-04-09 11:27:27 +0400

    Fixed issue with failed OSTF tests
    Change-Id: I428ece5d7e0cda6c7c8eb91623018d95b9e4998d
    Related-Bug: #1264292

--- fuel_health/murano.py
+++ fuel_health/murano.py
@@ -34,6 +34,15 @@ class MuranoTest(fuel_health.nmanager.OfficialClientTest):
         super(MuranoTest, self).setUp()
         self.check_clients_state()
         self.env_name = rand_name("ost1_test-Murano_env")
+
+        if not self.config.compute.compute_nodes:
+            self.fail('There are no compute nodes')
+
+        self.flavor_name = rand_name("ost1_test_Murano")
+        self.flavor = self.compute_client.flavors.create(self.flavor_name,
+                                                         disk=60, ram=2,
+                                                         vcpus=1)
+
         self.murano_available = True
         try:
             self.murano_client.environments.list()
@@ -49,6 +58,9 @@ class MuranoTest(fuel_health.nmanager.OfficialClientTest):
             after the Murano OSTF tests
         """
         super(MuranoTest, self).tearDown()
+
+        self.compute_client.flavors.delete(self.flavor.id)
+
         if self.murano_available:
             for env in self.list_environments():
                 if self.env_name in env.name:
