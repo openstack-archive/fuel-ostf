ae0e731b5dea38b59523f843272b3ddccf5751a2
tatyana-leontovich <tleontovich@mirantis.com>
2014-05-26 23:16:56 +0000

    Revert "Increase the count of ping probes"
    This reverts commit c4da245bc4b5691f1d6223218098b1449371de72.
    
    Change-Id: Ie0ec16a475398b28040763a94c5b5e4e484b0968

--- fuel_health/nmanager.py
+++ fuel_health/nmanager.py
@@ -540,7 +540,8 @@ class NovaNetworkScenarioTest(OfficialClientTest):
 
     def _ping_ip_address(self, ip_address, timeout, retries):
         def ping():
-            cmd = "ping -q -c3 -w10 %s" % ip_address
+            cmd = "ping -q -c3 -w3 %s | grep 'received' |" \
+                  " grep -v '0 packets received'" % ip_address
 
             if self.host:
                 try:
@@ -581,7 +582,8 @@ class NovaNetworkScenarioTest(OfficialClientTest):
             except Exception as exc:
                 LOG.debug(traceback.format_exc())
 
-            command = "ping -q -c3 -w10 8.8.8.8"
+            command = "ping -q -c3 -w3 8.8.8.8 | grep 'received' |" \
+                      " grep -v '0 packets received'"
 
             return self.retry_command(retries[0], retries[1],
                                       ssh.exec_command_on_vm,
