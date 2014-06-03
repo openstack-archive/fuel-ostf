3ea2fefad10126264662c198e525db465793f7a2
Dima Shulyak <dshulyak@mirantis.com>
2014-06-03 17:15:28 +0300

    Remove not used code
    this code is not used for a while, and i dont think it will be used
    
    Closes-Bug: #1326353
    Change-Id: I1d79454970dbb6f3484a1ea3e219898fc9e92b4e

--- fuel_plugin/testing/test_utils/db_manage.sh
+++
@@ -1,5 +0,0 @@
-export PGPASSWORD='ostf'
-psql -U postgres -h localhost -c "drop user adapter"
-psql -U postgres -h localhost -c "create role adapter with nosuperuser createdb password 'demo' login"
-psql -U postgres -h localhost -c "drop database if exists testing_adapter"
-psql -U postgres -h localhost -c "create database testing_adapter"
