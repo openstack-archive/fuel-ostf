4ce938a74c209fe5a0ab40390f3b78dfc8ba89a5
Anastasia Kuznetsova <akuznetsova@mirantis.com>
2014-07-29 21:01:48 +0400

    Skip ostf tests if available image is absent
    Need to skip heat "test_autoscaling" platform test and sahara
    "test_platform_savanna" test if available images are absent.
    (Now tests are fail in these cases.)
    
    Change-Id: I28d037b11902dfa422893b588a689df75cd644b3
    Closes-Bug: #1349390

--- fuel_health/tests/platform_tests/test_platform_savanna.py
+++ fuel_health/tests/platform_tests/test_platform_savanna.py
@@ -25,62 +25,63 @@ class PlatformSavannaTests(savanna.SavannaTest):
     """
     TestClass contains tests that check basic Savanna functionality.
     """
+    def setUp(self):
+        super(PlatformSavannaTests, self).setUp()
+        msg = ("Sahara image with tags wasn't imported into Glance, "
+               "please check "
+               "http://docs.mirantis.com/openstack/fuel/fuel-5.0/"
+               "user-guide.html#platform-tests-description")
+
+        if not self._test_image():
+            LOG.debug(msg)
+            self.skipTest(msg)
 
     def test_platform_savanna(self):
         """Sahara tests to create, list, start, checks and delete cluster
         Target component: Sahara
 
         Scenario:
-            1. Sahara image with tags should be imported
-            2. Send request to create node group template
-            3. Send request to create cluster template
-            4. Request the list of node group templates
-            5. Request the list of cluster templates
-            6. Send request to launch cluster
-            7. Send request to delete cluster
-            8. Send request to delete cluster template
-            9. Send request to delete node group templates
+            1. Send request to create node group template
+            2. Send request to create cluster template
+            3. Request the list of node group templates
+            4. Request the list of cluster templates
+            5. Send request to launch cluster
+            6. Send request to delete cluster
+            7. Send request to delete cluster template
+            8. Send request to delete node group templates
         Duration:  100 m.
 
         Deployment tags: Sahara
         """
-        fail_msg = ("Sahara image with tags wasn't imported into Glance, "
-                    "please check "
-                    "http://docs.mirantis.com/openstack/fuel/fuel-5.0/"
-                    "user-guide.html#platform-tests-description")
-        self.verify_response_true(
-            self.verify(30, self._test_image, 1, fail_msg,
-                        "Test images with tags"),
-            "Step 1 failed: {msg}".format(msg=fail_msg))
 
         fail_msg = 'Fail create node group template.'
-        self.verify(40, self.create_node_group_template_tt_dn, 2, fail_msg,
+        self.verify(40, self.create_node_group_template_tt_dn, 1, fail_msg,
                     "Create node group templates")
 
         fail_msg = 'Fail create cluster template.'
         cluster_template = self.verify(40, self.create_tiny_cluster_template,
-                                       3, fail_msg, "Create cluster templates")
+                                       2, fail_msg, "Create cluster templates")
 
         fail_msg = 'Fail list group templates.'
-        self.verify(40, self._list_node_group_template, 4, fail_msg,
+        self.verify(40, self._list_node_group_template, 3, fail_msg,
                     "List group templates")
 
         fail_msg = 'Fail list cluster templates.'
-        self.verify(40, self._list_cluster_templates, 5, fail_msg,
+        self.verify(40, self._list_cluster_templates, 4, fail_msg,
                     "List cluster templates")
 
         fail_msg = 'Fail launch cluster.'
-        self.verify(5400, self.create_sahara_cluster, 6, fail_msg,
+        self.verify(5400, self.create_sahara_cluster, 5, fail_msg,
                     "Launch cluster", cluster_template.id)
 
         fail_msg = 'Fail delete cluster.'
-        self.verify(40, self._clean_clusters, 7, fail_msg,
+        self.verify(40, self._clean_clusters, 6, fail_msg,
                     "Delete cluster")
 
         fail_msg = 'Fail delete cluster template.'
-        self.verify(40, self._clean_cluster_templates, 8, fail_msg,
+        self.verify(40, self._clean_cluster_templates, 7, fail_msg,
                     "Delete cluster template")
 
         fail_msg = 'Fail delete node group  templates.'
-        self.verify(40, self._clean_node_groups_templates, 9, fail_msg,
+        self.verify(40, self._clean_node_groups_templates, 8, fail_msg,
                     "Delete node group templates")
