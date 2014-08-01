0fe7bf8ee7e0c8f64fa3fce4c77ab825a9692bc3
Yaroslav Lobankov <ylobankov@mirantis.com>
2014-08-01 15:53:59 +0400

    Changes for Sahara and Murano OSTF tests
    We should allow running Sahara tests only when compute node has
    at least 4G RAM. That should greatly reduce number of errors. So this
    limit was added to OSTF tests for Sahara.
    
    Method for checking minimal free RAM of compute node was created and
    placed in class PlatformServicesBaseClass. Now OSTF tests for Murano
    use this method.
    
    Closes-Bug: #1334700
    
    Change-Id: Ic9333edc73c668ea5b953bb9dfa7c209fe2e258a

--- fuel_health/tests/platform_tests/test_platform_murano_linux.py
+++ fuel_health/tests/platform_tests/test_platform_murano_linux.py
@@ -37,12 +37,11 @@ class MuranoDeployLinuxServicesTests(murano.MuranoTest):
         self.check_clients_state()
 
         if not self.flavor_reqs:
-            self.skipTest("This test requires more resources "
-                          "on one of the compute nodes "
-                          "(>2048MB of free RAM), but you have "
-                          "only {0} MB of free RAM "
-                          "on most appropriate "
-                          "compute node. ".format(self.max_available_ram))
+            self.skipTest("This test requires more resources on one of "
+                          "the compute nodes (> {0} MB of free RAM), but you "
+                          "have only {1} MB on most appropriate compute node."
+                          .format(self.min_required_ram,
+                                  self.max_available_ram))
 
         msg = ("Linux image with Murano "
                "tag isn't available. Need to import this image into "
