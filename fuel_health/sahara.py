a1334dcc5776e417370aaa8f5ad68359399ecbdb
Yaroslav Lobankov <ylobankov@mirantis.com>
2014-08-28 15:14:20 +0400

    Fix for missing notification in Sahara OSTF tests
    Change-Id: I083eaac15748422c4b2713ab3a42064d68c12c4d
    Closes-Bug: #1342639

--- fuel_health/sahara.py
+++ fuel_health/sahara.py
@@ -74,6 +74,17 @@ class SaharaTest(nmanager.PlatformServicesBaseClass):
         self.check_clients_state()
         self._create_sahara_flavors(self.compute_client)
 
+    def get_tcp_rules_for_default_security_group(self):
+        # It is supposed that there is only one security group "default" after
+        # Open Stack is installed.
+        default_security_group = self.compute_client.security_groups.list()[0]
+        rules = []
+        for rule in default_security_group.rules:
+            if rule['ip_protocol'] == 'tcp':
+                rules.append(rule)
+
+        return rules
+
     def _test_image(self):
         LOG.debug('Testing image for plugin "%s" (Hadoop version %s)'
                   % (self.plugin, self.plugin_version))
