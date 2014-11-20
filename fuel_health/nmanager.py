6896b4c3c38ca3ae5d99633775ea7e4279f40715
vrovachev <vrovachev@mirantis.com>
2014-11-20 19:42:34 +0400

    Add ceilometer sahara notifications tests to ostf:
    Added ceilometer tests for sahara notifications:
    cluster.create, cluster.delete
    
    Partially implements: blueprint ceilometer-ostf-notification-tests
    
    Change-Id: I9175b6d0c090d4b664b2f59d03f94219e0fa111b

--- fuel_health/nmanager.py
+++ fuel_health/nmanager.py
@@ -713,6 +713,24 @@ class NovaNetworkScenarioTest(OfficialClientTest):
 
 class PlatformServicesBaseClass(NovaNetworkScenarioTest):
 
+    def setUp(self):
+        super(PlatformServicesBaseClass, self).setUpClass()
+        self.neutron_private_network_id = None
+        self.neutron_external_network_id = None
+        self.floating_ip_pool = None
+        if self.config.network.network_provider == 'neutron':
+            for network in self.neutron_client.list_networks()["networks"]:
+                if not network.get("router:external"):
+                    self.neutron_private_network_id = network['id']
+            for network in self.neutron_client.list_networks()["networks"]:
+                if network.get("router:external"):
+                    self.neutron_external_network_id = network['id']
+        else:
+            if not self.config.compute.auto_assign_floating_ip:
+                flip_list = self.compute_client.floating_ip_pools.list()
+                self.floating_ip_pool = next(
+                    flip.name for flip in flip_list if flip.is_loaded())
+
     def _try_port(self, host, port):
         start_time = time.time()
         delta = time.time() - start_time
