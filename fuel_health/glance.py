1cdf5e358713c00a69e2375f7b54f23e7febc115
Viktor Ryzhenkin <vryzhenkin@mirantis.com>
2014-12-16 12:40:39 +0300

    Add OSTF tests for Glance component
    - Added base class for Glance component in OSTF.
    - Function _list_images from test_sanity_compute.py now using
      Glance
    - Added 'Image Create' and 'List Images' tests for
      Glance api v1 and Glance api v2
    - Updated requirements.txt. Added python-glanceclient>=0.14.2
      and updated requests>=2.4.3
    - Added initialization of glance_client v1 and v2 in nmanager.py
    
    Partially implements: blueprint ostf-and-system-tests-for-glance
    
    Change-Id: I4a2957eefb98d718e7fc25153a6b07251250f4d2

---
+++ fuel_health/glance.py
@@ -0,0 +1,73 @@
+# Copyright 2014 Mirantis, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import logging
+
+from fuel_health.common.utils.data_utils import rand_name
+import fuel_health.nmanager
+import fuel_health.test
+import fuel_health.common.ssh
+
+
+LOG = logging.getLogger(__name__)
+
+
+class GlanceTest(fuel_health.nmanager.NovaNetworkScenarioTest):
+    """
+    Manager that provides access to the Glance python client for
+    calling Glance API.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super(GlanceTest, cls).setUpClass()
+        cls.images = []
+        if cls.manager.clients_initialized:
+            if not cls.glance_client:
+                LOG.warning('Glance client v1 was not initialized')
+            if not cls.glance_client_v2:
+                LOG.warning('Glance client v2 was not initialized')
+            if not cls.glance_client and not cls.glance_client_v2:
+                cls.fail('Glance client v1 and v2 was not initialized')
+
+    def tearDown(self):
+        LOG.debug("Deleting images created by Glance test")
+        self._clean_images()
+        super(GlanceTest, self).tearDown()
+
+    @staticmethod
+    def _list_images(client):
+        return client.images.list()
+
+    def image_create(self, client, **kwargs):
+        container_format = 'bare'
+        location = 'http://download.cirros-cloud.net/0.3.3/' \
+                   'cirros-0.3.3-x86_64-disk.img'
+        disk_format = 'qcow2'
+        image_name = rand_name('ostf_test-image_glance-')
+        image = client.images.create(name=image_name,
+                                     container_format=container_format,
+                                     location=location,
+                                     disk_format=disk_format, **kwargs)
+        self.images.append(image)
+        return image
+
+    def find_image_by_id(self, client, image_id):
+        image = client.images.get(image_id)
+        return image
+
+    def delete_image_by_id(self, client, object):
+        return client.images.delete(object)
---
+++ fuel_health/tests/smoke/test_create_images.py
@@ -0,0 +1,71 @@
+# Copyright 2014 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+from fuel_health import glance
+
+LOG = logging.getLogger(__name__)
+
+
+class GlanceSmokeTests(glance.GlanceTest):
+    """
+    Test suite verifies:
+    - image creation
+    - image deletion
+    """
+
+    def test_create_and_delete_image(self):
+        """Check that user can create and delete image using Glance v1
+        Target component: Glance
+
+        Scenario:
+            1.Create image
+            2.Check that image created successfully
+            3.Delete image
+        Duration: 90 s.
+        """
+        fail_msg = "Error creating image. Please refer to logs."
+        self.image = self.verify(60, self.image_create, 1, fail_msg,
+                                 'Image creation', self.glance_client)
+
+        fail_msg = "Image don't appear at list. Please refer to logs"
+        self.verify(10, self.find_image_by_id, 2, fail_msg, 'Finding image',
+                    self.glance_client, self.image.id)
+
+        fail_msg = 'Cant delete image. Please refer to logs.'
+        self.verify(20, self.delete_image_by_id, 3, fail_msg, 'Deleting image',
+                    self.glance_client, self.image)
+
+    def test_create_and_delete_image_v2(self):
+        """Check that user can create and delete image using Glance v2
+        Target component: Glance
+
+        Scenario:
+            1.Create image
+            2.Check that image created successfully
+            3.Delete image
+        Duration: 90 s.
+        """
+        fail_msg = "Error creating image. Please refer to logs."
+        self.image = self.verify(60, self.image_create, 1, fail_msg,
+                                 'Image creation', self.glance_client_v2)
+
+        fail_msg = "Image don't appear at list. Please refer to logs"
+        self.verify(10, self.find_image_by_id, 2, fail_msg, 'Finding image',
+                    self.glance_client_v2, self.image.id)
+
+        fail_msg = 'Cant delete image. Please refer to logs.'
+        self.verify(20, self.delete_image_by_id, 3, fail_msg, 'Deleting image',
+                    self.glance_client_v2, self.image.id)
