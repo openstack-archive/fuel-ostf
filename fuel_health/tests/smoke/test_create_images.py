2766f6e75b110b06a48841444c81f66c267d85ac
Viktor Ryzhenkin <vryzhenkin@mirantis.com>
2014-12-16 12:40:39 +0300

    Add OSTF tests for Glance component
    - Added base class for Glance component in OSTF.
    - Function _list_images removed from test_sanity_compute.py now using
      Glance
    - Added 'Image Create, Update, Delete' and 'List Images' tests for
      Glance api v1 and Glance api v2
    - Updated requirements: added python-glanceclient=>0.14.1
    - Added initialization of glance_client v1 and v2 in nmanager.py
    
    Partially implements: blueprint ostf-and-system-tests-for-glance
    
    Change-Id: I4a2957eefb98d718e7fc25153a6b07251250f4d2

---
+++ fuel_health/tests/smoke/test_create_images.py
@@ -0,0 +1,116 @@
+# Copyright 2014 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+from fuel_health import glance
+from fuel_health.common.utils.data_utils import rand_name
+
+LOG = logging.getLogger(__name__)
+
+
+class GlanceSmokeTests(glance.GlanceTest):
+    """
+    Test suite verifies:
+    - image creation
+    - image update
+    - image deletion
+    """
+
+    def test_create_and_delete_image(self):
+        """Check that user can create, update and delete image using Glance v1
+        Target component: Glance
+
+        Scenario:
+            1.Send request to create image via URL
+            2.Checking image status
+            3.Check that image created successfully
+            4.Update image with properties
+            5.Check properties updated successfully
+            6.Delete image
+        Duration: 180 s.
+        """
+        fail_msg = "Error creating image. Please refer to logs."
+        self.image = self.verify(10, self.image_create, 1, fail_msg,
+                                 'Image creation', self.glance_client)
+
+        fail_msg = "Image status incorrect. Please, check your " \
+                   "internet connection and refer to logs"
+        self.verify(120, self.check_image_status, 2, fail_msg,
+                    'Checking image status', self.glance_client, self.image)
+
+        fail_msg = "Image don't appear at list. Please refer to logs"
+        self.verify(10, self.find_image_by_id, 3, fail_msg, 'Finding image',
+                    self.glance_client, self.image.id)
+
+        group_props = rand_name("ostf_test")
+        prop = rand_name("ostf-prop")
+        value_prop = rand_name("prop-value")
+
+        fail_msg = "Can't update image with properties. Please refer to logs"
+        self.verify(10, self.update_image, 4, fail_msg, 'Updating image',
+                    self.glance_client, self.image, group_props, prop,
+                    value_prop)
+
+        fail_msg = "Can't find appended properties. Please refer to logs"
+        self.verify(10, self.find_props, 5, fail_msg, 'Finding properties',
+                    self.glance_client, self.image, group_props, prop,
+                    value_prop)
+
+        fail_msg = "Cant delete image. Please refer to logs."
+        self.verify(20, self.delete_image, 5, fail_msg, 'Deleting image',
+                    self.glance_client, self.image)
+
+    def test_create_and_delete_image_v2(self):
+        """Check that user can create, update and delete image using Glance v2
+        Target component: Glance
+
+        Scenario:
+            1.Send request to create image
+            2.Checking image status
+            3.Check that image created successfully
+            4.Update image with properties
+            5.Check properties updated successfully
+            6.Delete image
+        Duration: 70 s.
+        """
+        fail_msg = "Error creating image. Please refer to logs."
+        self.image = self.verify(10, self.image_create, 1, fail_msg,
+                                 'Image creation', self.glance_client_v2)
+
+        fail_msg = "Image status incorrect. Please refer to logs"
+        self.verify(10, self.check_image_status, 2, fail_msg,
+                    'Checking image status', self.glance_client_v2, self.image)
+
+        fail_msg = "Image don't appear at list. Please refer to logs"
+        self.verify(10, self.find_image_by_id, 3, fail_msg, 'Finding image',
+                    self.glance_client_v2, self.image.id)
+
+        group_props = rand_name("ostf_test")
+        prop = rand_name("ostf-prop")
+        value_prop = rand_name("prop-value")
+
+        fail_msg = "Can't update image with properties. Please refer to logs"
+        self.image = self.verify(10, self.update_image, 4, fail_msg,
+                                 'Updating image', self.glance_client_v2,
+                                 self.image.id, group_props, prop, value_prop)
+
+        fail_msg = "Can't find appended properties. Please refer to logs"
+        self.verify(10, self.find_props, 5, fail_msg, 'Finding properties',
+                    self.glance_client_v2, self.image, group_props, prop,
+                    value_prop)
+
+        fail_msg = "Can't delete image. Please refer to logs."
+        self.verify(20, self.delete_image, 5, fail_msg, 'Deleting image',
+                    self.glance_client_v2, self.image.id)
