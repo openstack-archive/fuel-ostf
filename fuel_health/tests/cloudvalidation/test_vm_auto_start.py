bf06a77b9008c9253fc9883e958e35ecacc9658b
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2015-02-19 16:47:54 +0200

    Added test for VM automatic start on host boot
    Reasons:
        - VMs should not start automatically on host boot
    Changes:
        - Added test for VM automatic start
    
    Implements blueprint: vm-auto-start
    
    Change-Id: I18c3d14953434378b578928f2f5672cf51ecda18

---
+++ fuel_health/tests/cloudvalidation/test_vm_auto_start.py
@@ -0,0 +1,55 @@
+# Copyright 2015 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from fuel_health import cloudvalidation
+
+
+class VMBootTest(cloudvalidation.CloudValidationTest):
+    """Cloud Validation Test class for VMs."""
+
+    def _check_host_boot(self, host):
+        """Test resume_guest_state_on_host_boot option on compute node.
+            By default, this option is set to False.
+        """
+
+        err_msg = ('The option "resume_guest_state_on_host_boot" '
+                   'is set to True at compute node {host}, so it can be broken'
+                   'down by the paused VMs after host boot.').format(host=host)
+
+        cmd = ('cat /etc/nova/nova.conf | '
+               'grep resume_guests_state_on_host_boot | '
+               'grep "^[^#]"')
+
+        out, err = self.verify(5, self._run_ssh_cmd, 1, err_msg,
+                               'check host boot option', host, cmd)
+
+        self.verify_response_true(not out and not err
+                                  or ('True' not in out
+                                  and 'True' not in err),
+                                  err_msg, 1)
+
+    def test_guests_state_on_host_boot(self):
+        """Check: host boot configuration on compute nodes
+        Target component: Nova
+
+        Scenario:
+            1. Check host boot configuration on compute nodes
+
+        Duration: 20 s.
+
+        Available since release: 2014.2-6.1
+        """
+
+        for host in self.computes:
+            self._check_host_boot(host)
