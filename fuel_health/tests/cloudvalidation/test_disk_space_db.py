687560000480b689dbb1739f331c293c608ec60f
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2015-02-19 16:47:54 +0200

    Added test for DB free space checking
    Reasons:
        - There should be no unused free space on controller nodes
    Changes:
        - Added test that checks if there is unused free space on
          controller nodes
    
    Implements blueprint: disk-space-db
    
    Change-Id: I63e41219890b549dc36ff4980cee84e06d110b2a

---
+++ fuel_health/tests/cloudvalidation/test_disk_space_db.py
@@ -0,0 +1,61 @@
+# Copyright 2015 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+from fuel_health import cloudvalidation
+
+LOG = logging.getLogger(__name__)
+
+
+class DBSpaceTest(cloudvalidation.CloudValidationTest):
+    """Cloud Validation Test class for free space for DB."""
+
+    def _check_db_disk_expectation_warning(self, host):
+        """Checks whether DB expects less free space than actually
+        is presented on the controller node
+        """
+
+        err_msg = "Cannot check nova-scheduler.log at {host}".format(host=host)
+        warning_msg = "Host has more disk space than database expected"
+        cmd = ''.join("cat /var/log/nova/nova-scheduler.log ",
+                      "| grep '{msg}' | tail -1").format(msg=warning_msg)
+
+        out, err = self.verify(5, self._run_ssh_cmd, 1, err_msg,
+                               'check nova-scheduler.log', host, cmd)
+
+        self.verify_response_true(not len(err), err_msg, 1)
+
+        return len(out)
+
+    def test_db_expectation_free_space(self):
+        """Check: disk space allocation for databases on controller nodes
+        Target component: Nova
+
+        Scenario:
+            1. Check disk space allocation for databases on controller nodes
+
+        Duration: 20 s.
+        """
+
+        hosts = list()
+
+        for host in self.controllers:
+            if self._check_db_disk_expectation_warning(host):
+                hosts.append(host)
+
+        self.verify_response_true(not len(hosts),
+                                  ''.join("Free disk space cannot be used ",
+                                          "by database on node(s)").format(
+                                  unicode(hosts)), 1)
