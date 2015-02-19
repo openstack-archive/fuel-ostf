1b9c6981e446d3f36dab2dcffb9a382da0996dd7
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2015-02-19 16:47:54 +0200

    Add logrotate check
    Add test that verifies whether
    logrotate is enabled on all compute
    and controller nodes
    
    Implements blueprint: log-rotation
    Change-Id: I86d224a60187d5f0fd03a5ac56f19f5fa180772c

---
+++ fuel_health/tests/cloudvalidation/__init__.py
@@ -0,0 +1,29 @@
+# Copyright 2015 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+'''
+Main purpose of following attribute is
+to supply general information about test set.
+This information will be stored in ostf database
+in test_sets table.
+'''
+__profile__ = {
+    "test_runs_ordering_priority": 5,
+    "id": "cloudv",
+    "driver": "nose",
+    "test_path": "fuel_health/tests/cloudv",
+    "cleanup_path": "fuel_health.cleanup",
+    "description": "Cloud validation tests. Duration 30 sec - 2 min",
+    "exclusive_testsets": []
+}
