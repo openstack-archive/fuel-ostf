cda1635fec01c0c008aa32998ccb489a911884ec
Igor Zinovik <izinovik@mirantis.com>
2015-03-16 12:21:37 +0300

    Replace _create_nano_flavor() with create_micro_flavor()
    - method _create_nano_flavor() was renamed to create_micro_flavor() in
      3535878efe441a80eb94089553ba7776d013c24a, so adapt vCenter related
      OSTF tests to this change
    
    Change-Id: I4a9d8fb083706d852f66d7a8eb72fb749a044053
    Closes-bug: #1431758

--- fuel_health/tests/smoke/test_vcenter.py
+++ fuel_health/tests/smoke/test_vcenter.py
@@ -252,7 +252,7 @@ class TestVcenterImageAction(nmanager.SmokeChecksTest):
     def setUpClass(cls):
         super(TestVcenterImageAction, cls).setUpClass()
         if cls.manager.clients_initialized:
-            cls.smoke_flavor = cls._create_nano_flavor()
+            cls.micro_flavors = cls.find_micro_flavor()
 
     @classmethod
     def tearDownClass(cls):
@@ -295,11 +295,10 @@ class TestVcenterImageAction(nmanager.SmokeChecksTest):
         test.call_until_true(is_deletion_complete, 10, 1)
 
     def _boot_image(self, image_id):
-        if not self.smoke_flavor:
-            self.fail("Flavor for tests was not created. Seems that "
+        if not self.micro_flavors:
+            self.fail("Flavor for tests was not found. Seems that "
                       "something is wrong with nova services.")
 
-        flavor_id = self.smoke_flavor
         name = rand_name('ost1_test-image')
         client = self.compute_client
         LOG.debug("name:%s, image:%s" % (name, image_id))
@@ -319,11 +318,12 @@ class TestVcenterImageAction(nmanager.SmokeChecksTest):
                           format(self.private_net))
             server = client.servers.create(name=name,
                                            image=image_id,
-                                           flavor=flavor_id, **create_kwargs)
+                                           flavor=micro_flavors[0],
+                                           **create_kwargs)
         else:
             server = client.servers.create(name=name,
                                            image=image_id,
-                                           flavor=flavor_id)
+                                           flavor=micro_flavors[0])
         self.set_resource(name, server)
         # self.addCleanup(self.compute_client.servers.delete, server)
         self.verify_response_body_content(
