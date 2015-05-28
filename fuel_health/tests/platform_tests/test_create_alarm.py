fff51cf17655687ad01083950928638f158ec43b
Yaroslav Lobankov <ylobankov@mirantis.com>
2015-05-28 20:54:05 +0300

    Editing all text messages in all tests for Ceilometer
    1. All text messages were edited so that users can understand
       why the test failed or was skipped.
    2. Some spelling errors were fixed.
    3. Some tests had incomplete description of scenario. Missing steps
       were added.
    
    Closes-Bug: #1459994
    
    Change-Id: I8bdf8e74601a47f83f87ef2342e23593f13992fe

--- fuel_health/tests/platform_tests/test_create_alarm.py
+++ fuel_health/tests/platform_tests/test_create_alarm.py
@@ -20,35 +20,33 @@ class CeilometerApiSmokeTests(ceilometermanager.CeilometerBaseTest):
     """TestClass contains tests that check basic Ceilometer functionality."""
 
     def test_create_alarm(self):
-        """Ceilometer create, update, check, delete alarm
+        """Ceilometer test to create, update, check and delete alarm
         Target component: Ceilometer
 
         Scenario:
-            1. Wait for statistic of metric.
-            2. Create a new alarm.
-            3. Get alarm.
+            1. Get statistic of metric.
+            2. Create an alarm.
+            3. Get the alarm.
             4. List alarms.
             5. Wait for 'ok' alarm state.
             6. Update the alarm.
             7. Wait for 'alarm' alarm state.
-            8. Get alarm history.
-            9. Change alarm state to 'ok'.
-            10. Verify state.
+            8. Get the alarm history.
+            9. Set the alarm state to 'insufficient data'.
+            10. Verify that the alarm state is 'insufficient data'.
             11. Delete the alarm.
+
         Duration: 120 s.
         Deployment tags: Ceilometer
         """
 
-        fail_msg = "Getting statistic of metric is failed."
-        msg = "Getting statistic of metric is successful."
-
+        fail_msg = 'Failed to get statistic of metric.'
+        msg = 'getting statistic of metric'
         self.verify(600, self.wait_for_statistic_of_metric, 1,
-                    fail_msg, msg,
-                    meter_name='image')
+                    fail_msg, msg, meter_name='image')
 
-        fail_msg = "Creation of alarm is failed."
-        msg = "Creation of alarm is successful."
-
+        fail_msg = 'Failed to create alarm.'
+        msg = 'creating alarm'
         alarm = self.verify(60, self.create_alarm, 2,
                             fail_msg, msg,
                             meter_name='image',
@@ -58,67 +56,50 @@ class CeilometerApiSmokeTests(ceilometermanager.CeilometerBaseTest):
                             statistic='avg',
                             comparison_operator='lt')
 
-        fail_msg = "Alarm getting is failed."
-        msg = "Alarm getting is successful."
-
+        fail_msg = 'Failed to get alarm.'
+        msg = 'getting alarm'
         self.verify(60, self.ceilometer_client.alarms.get, 3,
                     fail_msg, msg, alarm.alarm_id)
 
-        fail_msg = 'Getting alarms is failed.'
-        msg = 'Getting alarms is successful.'
+        fail_msg = 'Failed to list alarms.'
+        msg = 'listing alarms'
         query = [{'field': 'project', 'op': 'eq', 'value': alarm.project_id}]
-
         self.verify(60, self.ceilometer_client.alarms.list, 4,
                     fail_msg, msg, q=query)
 
-        fail_msg = "Alarm status is not equal 'ok'."
-        msg = "Alarm status is 'ok'."
-
+        fail_msg = 'Failed while waiting for alarm state to become "ok".'
+        msg = 'waiting for alarm state to become "ok"'
         self.verify(1000, self.wait_for_alarm_status, 5,
-                    fail_msg, msg,
-                    alarm.alarm_id, 'ok')
+                    fail_msg, msg, alarm.alarm_id, 'ok')
 
-        fail_msg = "Alarm update failed."
-        msg = "Alarm was updated."
-
+        fail_msg = 'Failed to update alarm.'
+        msg = 'updating alarm'
         self.verify(60, self.ceilometer_client.alarms.update, 6,
-                    fail_msg, msg,
-                    alarm_id=alarm.alarm_id,
-                    threshold=1.1)
+                    fail_msg, msg, alarm_id=alarm.alarm_id, threshold=1.1)
 
-        fail_msg = "Alarm verify state is failed."
-        msg = "Alarm status is 'alarm'."
-
+        fail_msg = 'Failed while waiting for alarm state to become "alarm".'
+        msg = 'waiting for alarm state to become "alarm"'
         self.verify(1000, self.wait_for_alarm_status, 7,
-                    fail_msg, msg,
-                    alarm.alarm_id, 'alarm')
+                    fail_msg, msg, alarm.alarm_id, 'alarm')
 
-        fail_msg = "Getting history of alarm is failed."
-        msg = 'Getting alarms history is successful.'
-
+        fail_msg = 'Failed to get alarm history.'
+        msg = 'getting alarm history'
         self.verify(60, self.ceilometer_client.alarms.get_history, 8,
-                    fail_msg, msg,
-                    alarm_id=alarm.alarm_id)
+                    fail_msg, msg, alarm_id=alarm.alarm_id)
 
-        fail_msg = "Setting alarm state to 'insufficient data' is failed."
-        msg = "Set alarm state to 'insufficient data'."
-
+        fail_msg = 'Failed to set alarm state to "insufficient data".'
+        msg = 'setting alarm state to "insufficient data"'
         self.verify(60, self.ceilometer_client.alarms.set_state, 9,
-                    fail_msg, msg,
-                    alarm_id=alarm.alarm_id,
+                    fail_msg, msg, alarm_id=alarm.alarm_id,
                     state='insufficient data')
 
-        fail_msg = "Alarm state verification is failed."
-        msg = "Alarm state verification is successful."
-
+        fail_msg = 'Failed while verifying alarm state.'
+        msg = 'verifying alarm state'
         self.verify(60, self.verify_state, 10,
-                    fail_msg, msg,
-                    alarm_id=alarm.alarm_id,
+                    fail_msg, msg, alarm_id=alarm.alarm_id,
                     state='insufficient data')
 
-        fail_msg = "Alarm deleting is failed."
-        msg = "Alarm deleted."
-
+        fail_msg = 'Failed to delete alarm.'
+        msg = 'deleting alarm'
         self.verify(60, self.ceilometer_client.alarms.delete, 11,
-                    fail_msg, msg,
-                    alarm_id=alarm.alarm_id)
+                    fail_msg, msg, alarm_id=alarm.alarm_id)
