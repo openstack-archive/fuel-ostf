9be10547c019ca3c450da0576a21e7acd43a52c2
Tatyana Leontovich <tleontovich@mirantis.com>
2015-06-15 13:32:27 +0300

    Rename platfrom tests to keep run ordering
    Node plagin run tests in adc order,
    as result platfrom tests runs before smoke, that is wrong
    
    So  platfrom_test to tests_platfrom
    to get smoke tests executedbefore platform
    
    Closes-Bug: #1465238
    Change-Id: I91774614afb7edc0ecec3f5b592b75db224ac543

--- fuel_health/tests/smoke/__init__.py
+++ fuel_health/tests/smoke/__init__.py
@@ -27,5 +27,5 @@ __profile__ = {
     "test_path": "fuel_health/tests/smoke",
     "cleanup_path": "fuel_health.cleanup",
     "description": "Functional tests. Duration 3 min - 14 min",
-    "exclusive_testsets": ['smoke_platform']
+    "exclusive_testsets": ['smoke_tests']
 }
