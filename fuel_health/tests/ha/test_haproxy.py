c9c1c78cbd6892ccf6574c2a73321fbed431bb56
Dmitry Tyzhnenko <dtyzhnenko@mirantis.com>
2015-06-24 18:51:03 +0300

    Add HAProxy checker into HA
    Check HAProxy backend states
    
    Change-Id: I59acef38618ece2e3e6fa756d274791f5b67204e
    Related-bug: #1439652
    Closes-bug: #1433162

---
+++ fuel_health/tests/ha/test_haproxy.py
@@ -0,0 +1,97 @@
+# Copyright 2015 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+from fuel_health.common.ssh import Client as SSHClient
+from fuel_health import test
+
+
+LOG = logging.getLogger(__name__)
+
+
+class HAProxyCheck(test.BaseTestCase):
+    """TestClass contains HAProxy checks."""
+    @classmethod
+    def setUpClass(cls):
+        super(HAProxyCheck, cls).setUpClass()
+        cls.controllers = cls.config.compute.online_controllers
+        cls.controller_key = cls.config.compute.path_to_private_key
+        cls.controller_user = cls.config.compute.ssh_user
+
+    def setUp(self):
+        super(HAProxyCheck, self).setUp()
+        if not self.controllers:
+            self.skipTest('There are no controller nodes')
+
+    def _check_haproxy_backend(self, remote,
+                               services=None, nodes=None,
+                               ignore_services=None, ignore_nodes=None):
+        """Check DOWN state of HAProxy backends. Define names of service or
+        nodes if need check some specific service or node. Use ignore_services
+        for ignore service status on all nodes. Use ignore_nodes for ignore all
+        services on all nodes. Ignoring has a bigger priority.
+        :type remote: SSHClient
+        :type service: List
+        :type nodes: List
+        :type ignore_services: List
+        :type ignore_nodes: List
+            :rtype: Dict
+        """
+        cmd = 'haproxy-status.sh | egrep -v "BACKEND|FRONTEND"'
+
+        pos_filter = (services, nodes)
+        neg_filter = (ignore_services, ignore_nodes)
+        grep = ['|egrep "{0}"'.format('|'.join(n)) for n in pos_filter if n]
+        grep.extend(
+            ['|egrep -v "{0}"'.format('|'.join(n)) for n in neg_filter if n])
+
+        return remote.exec_command("{0}{1}".format(cmd, ''.join(grep)))
+
+    def test_001_check_state_of_backends(self):
+        """Check backends state on controllers
+        Target Service: HAProxy
+
+        Scenario:
+            1. Ssh on each controller and get state of HAProxy backends
+            2. Check backend state for availability
+        Duration: 10 s.
+        Available since release: 2014.2.2-6.1
+        """
+        LOG.info("Controllers nodes are %s" % self.controllers)
+        for controller in self.controllers:
+            remote = SSHClient(controller, self.controller_user,
+                               key_filename=self.controller_key,
+                               timeout=100)
+            if 'neutron' in self.config.network.network_provider:
+                haproxy_status = self.verify(
+                    10, self._check_haproxy_backend,
+                    1,
+                    "Can't get state of backends.",
+                    "Getting state of backends",
+                    remote)
+            else:
+                haproxy_status = self.verify(
+                    10, self._check_haproxy_backend,
+                    1,
+                    "Can't get state of backends.",
+                    "Getting state of backends",
+                    remote,
+                    ignore_services=['nova-metadata-api'])
+            dead_backends = filter(lambda x: 'DOWN' in x,
+                                   haproxy_status.splitlines())
+            self.verify_response_true(
+                len(dead_backends) == 0,
+                "Step 2 failed: Controller has down "
+                "backend {0}".format(dead_backends))
