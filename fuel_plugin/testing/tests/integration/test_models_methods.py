06a32dbef6a809abd5127d59aa1e9bb72fec650b
Artem Roma <aroma@mirantis.com>
2015-09-16 11:49:23 +0300

    Add tests for methods of Test model class
    Change-Id: Iaedb7120d8620b43dcc0ce8169fda1d0c4f804f8
    Partial-Bug: #1435229

---
+++ fuel_plugin/testing/tests/integration/test_models_methods.py
@@ -0,0 +1,186 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+''' Since our orm models classes has word 'Test' in
+their names they cannot be imported at the module's
+level otherwise these entities will be discovered by
+nosetest engine and counted as test suits to be
+executed and that obviously leads to errors.
+
+To avoid the situation imports of model classes
+are done inside of methods of actual test class here.
+
+Please, note that such behavior is observed only for
+nosetest, using pytest as tests's executor doesn't produce
+such problem
+'''
+
+import six
+
+from fuel_plugin.testing.tests import base
+
+from fuel_plugin.ostf_adapter import mixins
+
+
+class TestModelTestMethods(base.BaseIntegrationTest):
+
+    def setUp(self):
+        super(TestModelTestMethods, self).setUp()
+
+        self.discovery()
+
+        self.test_set_id = 'general_test'
+        self.cluster_id = 1
+
+        self.mock_api_for_cluster(self.cluster_id)
+
+        mixins.discovery_check(self.session, self.cluster_id)
+        self.session.flush()
+
+        # FIXME(aroma): remove these imports when pytest will be
+        # default testing framework for ostf
+        from fuel_plugin.ostf_adapter.storage.models import Test
+        from fuel_plugin.ostf_adapter.storage.models import TestRun
+
+        self.test_obj = self.session.query(Test)\
+            .filter_by(test_set_id=self.test_set_id)\
+            .first()
+
+        self.test_run = TestRun.add_test_run(
+            self.session,
+            self.test_obj.test_set_id,
+            self.cluster_id,
+            status='running',
+            tests=[self.test_obj.name]
+        )
+        self.session.flush()
+
+    @property
+    def test_to_check(self):
+        # FIXME(aroma): remove this import when pytest will be
+        # default testing framework for ostf
+        from fuel_plugin.ostf_adapter.storage.models import Test
+        return self.session.query(Test)\
+            .filter_by(test_run_id=self.test_run.id)\
+            .filter_by(name=self.test_obj.name)\
+            .first()
+
+    def check_model_obj_attrs(self, obj, attrs):
+        for attr_name, attr_val in six.iteritems(attrs):
+            self.assertEqual(attr_val, getattr(obj, attr_name))
+
+    def test_add_result(self):
+        expected_data = {
+            'message': 'test_message',
+            'status': 'error',
+            'time_taken': 10.4
+        }
+
+        # FIXME(aroma): remove this import when pytest will be
+        # default testing framework for ostf
+        from fuel_plugin.ostf_adapter.storage.models import Test
+        Test.add_result(self.session,
+                        self.test_run.id,
+                        self.test_obj.name,
+                        expected_data)
+
+        self.check_model_obj_attrs(self.test_to_check, expected_data)
+
+    def test_update_running_tests_default_status(self):
+        # FIXME(aroma): remove this import when pytest will be
+        # default testing framework for ostf
+        from fuel_plugin.ostf_adapter.storage.models import Test
+        Test.update_running_tests(self.session,
+                                  self.test_run.id)
+
+        self.assertEqual(self.test_to_check.status, 'stopped')
+
+    def test_update_running_tests_with_status(self):
+        expected_status = 'success'
+
+        # FIXME(aroma): remove this import when pytest will be
+        # default testing framework for ostf
+        from fuel_plugin.ostf_adapter.storage.models import Test
+        Test.update_running_tests(self.session,
+                                  self.test_run.id,
+                                  status=expected_status)
+
+        self.assertEqual(self.test_to_check.status, expected_status)
+
+    def test_update_only_running_tests(self):
+        # the method should update only running tests
+        expected_status = 'error'
+        # FIXME(aroma): remove this import when pytest will be
+        # default testing framework for ostf
+        from fuel_plugin.ostf_adapter.storage.models import Test as Model
+        Model.add_result(self.session, self.test_run.id,
+                         self.test_obj.name,
+                         {'status': expected_status})
+
+        Model.update_running_tests(self.session, self.test_run.id)
+
+        # check that status of test is not updated to 'stopped'
+        self.assertEqual(self.test_to_check.status, expected_status)
+
+    def test_update_test_run_tests_default_status(self):
+        # FIXME(aroma): remove this import when pytest will be
+        # default testing framework for ostf
+        from fuel_plugin.ostf_adapter.storage.models import Test as Model
+        # set 'time_taken' attribute to non-null value
+        Model.add_result(self.session, self.test_run.id,
+                         self.test_obj.name,
+                         {'time_taken': 10.4})
+
+        Model.update_test_run_tests(self.session, self.test_run.id,
+                                    [self.test_obj.name])
+
+        expected_attrs = {
+            'status': 'wait_running',
+            'time_taken': None
+        }
+
+        self.check_model_obj_attrs(self.test_to_check, expected_attrs)
+
+    def test_properly_copied_test(self):
+        new_test = self.test_obj.copy_test(self.test_run, predefined_tests=[])
+
+        copied_attrs_list = [
+            'name',
+            'title',
+            'description',
+            'duration',
+            'message',
+            'traceback',
+            'step',
+            'time_taken',
+            'meta',
+            'deployment_tags',
+            'available_since_release',
+            'test_set_id',
+        ]
+        attrs = {}
+        for attr_name in copied_attrs_list:
+            attrs[attr_name] = getattr(self.test_obj, attr_name)
+
+        self.check_model_obj_attrs(new_test, attrs)
+
+        self.assertEqual(new_test.test_run_id, self.test_run.id)
+        self.assertEqual(new_test.status, 'wait_running')
+
+    def test_copy_test_with_predefined_list(self):
+        predefined_tests_names = ['some_other_test']
+        new_test = self.test_obj.copy_test(self.test_run,
+                                           predefined_tests_names)
+
+        self.assertEqual(new_test.status, 'disabled')
