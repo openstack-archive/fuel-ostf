121dd046e2a1eff2d8cf2792db32361e420f6a07
Anton Arefiev <aarefiev@mirantis.com>
2015-09-30 15:39:29 +0300

    [WIP] Support ironicclient
    Change-Id: I43994a9a5523c739db0d0cc65fa6b3adc3b224ad

---
+++ fuel_health/ironicmanager.py
@@ -0,0 +1,54 @@
+# Copyright 2015 Mirantis, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+import fuel_health.common.ssh
+import fuel_health.nmanager
+import fuel_health.test
+
+
+LOG = logging.getLogger(__name__)
+
+
+class IronicTest(fuel_health.nmanager.NovaNetworkScenarioTest):
+    """Manager that provides access to the Ironic python client for
+    calling Ironic API.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super(IronicTest, cls).setUpClass()
+        cls.nodes = []
+        if cls.manager.clients_initialized:
+            if not cls.ironic_client:
+                LOG.warning('Ironic client was not initialized')
+
+    def tearDown(self):
+        LOG.debug("Deleting nodes created by Ironic test")
+        self._clean_nodes()
+        super(IronicTest, self).tearDown()
+
+    def _list_nodes(self, client):
+        return client.node.list()
+
+    def node_create(self, client, **kwargs):
+        node = client.node.create(**kwargs)
+        self.nodes.append(node.uuid)
+        return node
+
+    def delete_image(self, client, object):
+        client.node.delete(object)
+        return self.images.remove(object.uuid)
