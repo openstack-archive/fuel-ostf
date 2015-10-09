b0bb6a3c253253a4fd1e6a9fc9cf89ff3c3fd3c5
Kyrylo Romanenko <kromanenko@mirantis.com>
2015-10-09 17:13:26 +0300

    Add OSTF tests for Ironic using ironicclient
    Check Ironic services:
        - ironic-api on controller node;
        - ironic-conductor on ironic conductor node.
    Operate Ironic nodes via ironicclient api:
        - Create Ironic node with fake driver;
        - Update Ironic node properties;
        - Show and check updated node properties;
        - Delete Ironic node.
    List Ironic entities:
        - List chassis;
        - List drivers;
        - List nodes;
        - List ports.
    
    Implements: blueprint ironic-ostf-tests
    Change-Id: I90fafede030a243efac3c9eb8d840531b25d0d03
    Co-Authored-by: Anton Arefiev <aarefiev@mirantis.com>

---
+++ fuel_health/ironicmanager.py
@@ -0,0 +1,113 @@
+# Copyright 2015 Mirantis, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import traceback
+
+from fuel_health.common.ssh import Client as SSHClient
+import fuel_health.nmanager
+import fuel_health.test
+from ironicclient.common import utils
+
+LOG = logging.getLogger(__name__)
+
+
+class IronicTest(fuel_health.nmanager.NovaNetworkScenarioTest):
+    """Manager that provides access to the Ironic python client for
+    calling Ironic API.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super(IronicTest, cls).setUpClass()
+        if cls.manager.clients_initialized:
+            cls.usr = cls.config.compute.controller_node_ssh_user
+            cls.pwd = cls.config.compute.controller_node_ssh_password
+            cls.key = cls.config.compute.path_to_private_key
+            cls.timeout = cls.config.compute.ssh_timeout
+            if not cls.ironic_client:
+                LOG.warning('Ironic client was not initialized')
+
+    def node_create(self, **kwargs):
+        """Create a new node."""
+        node = self.ironic_client.node.create(**kwargs)
+        self.addCleanup(self.node_delete, node)
+        return node
+
+    def node_delete(self, node):
+        """Delete particular node."""
+        try:
+            self.ironic_client.node.delete(node.uuid)
+        except Exception:
+            LOG.debug(traceback.format_exc())
+
+    def node_update(self, node, prop, value_prop):
+        """Add property with value to node properties."""
+        args = ['properties/%s=%s' % (prop, value_prop)]
+        patch = utils.args_array_to_patch('add', args)
+        return self.ironic_client.node.update(node.uuid, patch)
+
+    def node_show(self, node):
+        """Show detailed information about a node."""
+        if node.instance_uuid:
+            n = self.ironic_client.node.get_by_instance_uuid(
+                node.instance_uuid)
+        else:
+            n = self.ironic_client.node.get(node.uuid)
+        return n
+
+    def _run_ssh_cmd_with_exit_code(self, host, cmd):
+        """Open SSH session with host and execute command.
+           Fail if exit code != 0
+        """
+        try:
+            sshclient = SSHClient(host, self.usr, self.pwd,
+                                  key_filename=self.key, timeout=self.timeout)
+            return sshclient.exec_command(cmd)
+        except Exception:
+            LOG.debug(traceback.format_exc())
+            self.fail("%s command failed." % cmd)
+
+    def check_service_availability(self, nodes, cmd, expected, timeout=30):
+        """Check running processes on nodes
+
+           At least one controller should run ironic-api process.
+           At least one Ironic node should run ironic-conductor process.
+        """
+
+        def check_services():
+            for node in nodes:
+                output = self._run_ssh_cmd_with_exit_code(node, cmd)
+                LOG.debug(output)
+                if expected in output:
+                    return True
+            return False
+
+        if not fuel_health.test.call_until_true(check_services, 30, timeout):
+            self.fail('Failed to discover service {0} '
+                      'within specified timeout'.format(expected))
+        return True
+
+    def list_nodes(self):
+        return self.ironic_client.node.list()
+
+    def list_ports(self):
+        return self.ironic_client.port.list()
+
+    def list_drivers(self):
+        return self.ironic_client.driver.list()
+
+    def list_chassis(self):
+        return self.ironic_client.chassis.list()
