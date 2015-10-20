71f401ee9986f94d537936c93cf85c50c0b349b3
Dmitry Teselkin <dteselkin@mirantis.com>
2015-10-20 12:44:23 +0300

    [WIP] Update requirements for centos7 ISO
    Change-Id: I0f537482eb33e3ca0afe87340868d6844513b4bf

--- fuel_plugin/ostf_adapter/nose_plugin/nose_adapter.py
+++ fuel_plugin/ostf_adapter/nose_plugin/nose_adapter.py
@@ -17,7 +17,7 @@ import logging
 import os
 import signal
 
-from oslo.config import cfg
+from oslo_config import cfg
 
 from fuel_plugin.ostf_adapter.logger import ResultsLogger
 from fuel_plugin.ostf_adapter.nose_plugin import nose_storage_plugin
