bcc4674e05d98ebe5c91b113f0a1bdb86e2a011f
Tatyana Leontovich <tleontovich@mirantis.com>
2015-11-16 19:44:08 +0200

    Fix gate job for ostf repo
    The thing is that nailgun tests require postgresql backend to
    be available. So, we need to prepare it before running tests.
    Fortunately, all unit tests are run on nodes where postgresql
    is installed.
    
    Change-Id: I8f93bb78cb4144f993fb55936e157b5fe153a4c1
    Closes-Bug: #1516609

---
+++ etc/tools/prepare_database.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Preparing pgpass file ${DB_ROOTPGPASS}"
+echo "*:*:*:${DB_ROOT}:${DB_ROOTPW}" > ${DB_ROOTPGPASS}
+chmod 600 ${DB_ROOTPGPASS}
+
+export PGPASSFILE=${DB_ROOTPGPASS}
+
+echo "Trying to find out if role ${OSTF_DB_USER} exists"
+root_roles=$(psql -U ${DB_ROOT} -t -c "SELECT 'HERE' from pg_roles where rolname='${OSTF_DB_USER}'")
+if [[ ${root_roles} == *HERE ]];then
+  echo "Role ${OSTF_DB_USER} exists. Setting password ${OSTF_DB_PW}"
+  psql -U ${DB_ROOT} -c "ALTER ROLE ${OSTF_DB_USER} WITH SUPERUSER LOGIN PASSWORD '${OSTF_DB_PW}'"
+else
+  echo "Creating role ${OSTF_DB_USER} with password ${OSTF_DB_PASSWD}"
+  psql -U ${DB_ROOT} -c "CREATE ROLE ${OSTF_DB_USER} WITH SUPERUSER LOGIN PASSWORD '${OSTF_DB_PW}'"
+fi
+
+echo "Dropping database ${OSTF_DB} if exists"
+psql  -U ${DB_ROOT} -c "DROP DATABASE IF EXISTS ${OSTF_DB}"
+echo "Creating database ${OSTF_DB}"
+psql  -U ${DB_ROOT} -c "CREATE DATABASE ${OSTF_DB} OWNER ${OSTF_DB_USER}"
---
+++ etc/tools/prepare_settings_yaml.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cat > ${OSTF_DB_CONF} <<EOL
+DEVELOPMENT: 1
+DATABASE:
+  name: "${OSTF_DB}"
+  engine: "postgresql"
+  host: "localhost"
+  port: "8777"
+  user: "${OSTF_DB_USER}"
+  passwd: "${OSTF_DB_PW}"
+  log_file: "${OSTF_LOGS}"
+
+EOL
