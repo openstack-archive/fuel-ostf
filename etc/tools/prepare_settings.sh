6390e813fe624bde244a8036fde962373824d0e4
Tatyana Leontovich <tleontovich@mirantis.com>
2015-11-16 19:44:08 +0200

    Fix gate job for ostf repo
    The thing is that nailgun tests require postgresql backend to
    be available. So, we need to prepare it before running tests.
    Fortunately, all unit tests are run on nodes where postgresql
    is installed.
    
    Change-Id: I8f93bb78cb4144f993fb55936e157b5fe153a4c1
    Closes-Bug: #1516609

---
+++ etc/tools/prepare_settings.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cat > ${OSTF_CI_CONF} <<EOL
+[adapter]
+server_host = 127.0.0.1
+server_port = 8777
+dbpath = postgresql+psycopg2://ostf:ostf@localhost/ostf
+lock_dir = /var/lock
+nailgun_host = 127.0.0.1
+nailgun_port = 8000
+log_file = ${OSTF_LOGS}"
+after_init_hook = False
+auth_enable = False
+name: "${OSTF_DB}"
+engine: "postgresql"
+user: "${OSTF_DB_USER}"
+passwd: "${OSTF_DB_PW}"
+EOL
