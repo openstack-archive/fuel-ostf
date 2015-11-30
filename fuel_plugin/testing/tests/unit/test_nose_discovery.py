91df8e2897b195840703131ff299a389bf60051d
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-11-30 09:25:52 +0000

    Compare versions of OpenStack with LooseVersion
    Since release Liberty, OpenStack doesn't have common release version
    (like 2015.1.0). If we use release codename to identify the version
    instead (like 'liberty'), it cannot be compared to previous numeric
    versions with distutils.version.StrictVersion.
    
    Use of LooseVersion instead ensures that alphabetic version will always
    be considered 'newer' than numeric version.
    
    Change-Id: Ifefc4fab56993ed369a92804b1a866e766b2ec0b
    Related-bug: 1503663

--- fuel_plugin/testing/tests/unit/test_nose_discovery.py
+++ fuel_plugin/testing/tests/unit/test_nose_discovery.py
@@ -201,6 +201,35 @@ class TestNoseDiscovery(base.BaseUnitTest):
             self.assertEqual(needed_test.available_since_release,
                              test['available_since_release'])
 
+    def test_alphabetic_version_attribute(self):
+        expected = {
+            'test_set': {
+                'id': 'test_alphabetic_versioning',
+                'available_since_release': 'liberty-8.0',
+            },
+            'tests': [
+                {'name': ('fuel_plugin.testing.fixture.dummy_tests.'
+                          'test_alphabetic_versioning.TestVersioning.'
+                          'test_simple_fake_first'),
+                 'available_since_release': '2015.2.0-6.0', },
+                {'name': ('fuel_plugin.testing.fixture.dummy_tests.'
+                          'test_alphabetic_versioning.TestVersioning.'
+                          'test_simple_fake_second'),
+                 'available_since_release': 'mitaka-8.0', }
+            ]
+        }
+
+        needed_test_set = self._find_needed_test_set(
+            expected['test_set']['id']
+        )
+        self.assertEqual(needed_test_set.available_since_release,
+                         expected['test_set']['available_since_release'])
+
+        for test in expected['tests']:
+            needed_test = self._find_needed_test(test['name'])
+            self.assertEqual(needed_test.available_since_release,
+                             test['available_since_release'])
+
     def test_description_parsing(self):
         test_obj = Mock(spec=case.Test(Mock()))
 
