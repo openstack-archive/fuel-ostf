0437457bf2e7f9762270e9d78438921e9ad2c8e5
Tatyana Leontovich <tleontovich@mirantis.com>
2015-12-17 00:46:01 +0200

    Switch OSTF to work with public endpoint
    To be sure that OSTF works fine with SSL,
    switch  to use public endpoints without
    validation of certificate
    
    Change-Id: I87a625899c1eb4b80376c8bdb5aca4fce3d8ad66
    Closes-Bug: #1486056
    Related-Bug: #1524850

--- fuel_health/tests/smoke/test_create_images.py
+++ fuel_health/tests/smoke/test_create_images.py
@@ -40,8 +40,10 @@ class GlanceSmokeTests(glancemanager.GlanceTest):
             6.Delete image
 
         Duration: 130 s.
+        Deployment tags: disabled
         Available since release: 2014.2-6.1
         """
+        # TODO (tleontovich) enable when mos LP1527224 fixed
         fail_msg = ("Error creating image. Please refer to Openstack logs "
                     "for more information.")
         self.image = self.verify(100, self.image_create, 1, fail_msg,
