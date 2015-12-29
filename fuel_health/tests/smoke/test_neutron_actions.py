ee90593e76cdb52fb7f0b477c569dfbfa16b62ac
Alexey Stepanov <astepanov@mirantis.com>
2015-12-29 12:59:04 +0300

    IP v6 support coverage in Smoke
    1. Add option: CIDR for IPv6
    2, Add test IPv6 network
    
    Related-bug: #1518979
    
    Change-Id: I4699f415785c22fafe2717a35152cab46db7a7b4

--- fuel_health/tests/smoke/test_neutron_actions.py
+++ fuel_health/tests/smoke/test_neutron_actions.py
@@ -39,20 +39,23 @@ class TestNeutron(neutronmanager.NeutronBaseTest):
             1. Create a new security group (if it doesn`t exist yet).
             2. Create router
             3. Create network
-            4. Create subnet
-            5. Uplink subnet to router.
-            6. Create an instance using the new security group
-               in created subnet.
-            7. Create a new floating IP
-            8. Assign the new floating IP to the instance.
-            9. Check connectivity to the floating IP using ping command.
-            10. Check that public IP 8.8.8.8 can be pinged from instance.
-            11. Disassociate server floating ip.
-            12. Delete floating ip
-            13. Delete server.
-            14. Remove router.
-            15. Remove subnet
-            16. Remove network
+            4. Create IPv4 subnet
+            5. Create IPv6 subnet
+            6. Uplink IPv4 subnet to router.
+            7. Uplink IPv6 subnet to router.
+            8. Create an instance using the new security group
+               in created subnets.
+            9. Create a new floating IP
+            10. Assign the new floating IP to the instance.
+            11. Check connectivity to the floating IP using ping command.
+            12. Check connectivity to the gateway_ip using ping6 command.
+            13. Check that public IP 8.8.8.8 can be pinged from instance.
+            14. Disassociate server floating ip.
+            15. Delete floating ip
+            16. Delete server.
+            17. Remove router.
+            18. Remove subnet
+            19. Remove network
 
         Duration: 300 s.
 
@@ -80,29 +83,36 @@ class TestNeutron(neutronmanager.NeutronBaseTest):
                               'Network can not be created',
                               'Network creation', name)
 
-        subnet = self.verify(20, self.create_subnet, 4,
-                             'Subnet can not be created',
-                             'Subnet creation', network)
+        subnet_v4 = self.verify(20, self.create_subnet_v4, 4,
+                                'Subnet v4 can not be created',
+                                'Subnet creation', network)
 
-        self.verify(20, self.uplink_subnet_to_router, 5,
-                    'Can not uplink subnet to router',
-                    'Uplink subnet to router', router, subnet)
+        subnet_v6 = self.verify(20, self.create_subnet_v6, 5,
+                                'Subnet v6 can not be created',
+                                'Subnet creation', network)
 
-        server = self.verify(200, self._create_server, 6,
+        self.verify(20, self.uplink_subnet_to_router, 6,
+                    'Can not uplink subnet v4 to router',
+                    'Uplink subnet to router', router, subnet_v4)
+
+        self.verify(20, self.uplink_subnet_to_router, 7,
+                    'Can not uplink subnet v6 to router',
+                    'Uplink subnet to router', router, subnet_v6)
+
+        server = self.verify(200, self._create_server, 8,
                              "Server can not be created.",
                              "server creation",
                              self.compute_client, name, security_groups,
                              net_id=network['id'])
 
-        floating_ip = self.verify(
-            20,
-            self._create_floating_ip,
-            7,
-            "Floating IP can not be created.",
-            'floating IP creation')
+        floating_ip = self.verify(20,
+                                  self._create_floating_ip,
+                                  9,
+                                  "Floating IP can not be created.",
+                                  'floating IP creation')
 
         self.verify(20, self._assign_floating_ip_to_instance,
-                    8, "Floating IP can not be assigned.",
+                    10, "Floating IP can not be assigned.",
                     'floating IP assignment',
                     self.compute_client, server, floating_ip)
 
@@ -112,35 +122,41 @@ class TestNeutron(neutronmanager.NeutronBaseTest):
         LOG.info('is address is  {0}'.format(ip_address))
         LOG.debug(ip_address)
 
-        self.verify(600, self._check_vm_connectivity, 9,
+        self.verify(600, self._check_vm_connectivity, 11,
                     "VM connectivity doesn`t function properly.",
                     'VM connectivity checking', ip_address,
                     30, (9, 60))
 
+        self.verify(600, self._check_vm_connectivity, 12,
+                    "VM connectivity doesn`t function properly.",
+                    'VM connectivity to gateway_ip checking',
+                    subnet_v6["gateway_ip"],
+                    30, (9, 60))
+
         self.verify(600, self._check_connectivity_from_vm,
-                    10, ("Connectivity to 8.8.8.8 from the VM doesn`t "
+                    13, ("Connectivity to 8.8.8.8 from the VM doesn`t "
                          "function properly."),
                     'public connectivity checking from VM', ip_address,
                     30, (9, 60))
 
         self.verify(20, self.compute_client.servers.remove_floating_ip,
-                    11, "Floating IP cannot be removed.",
+                    14, "Floating IP cannot be removed.",
                     "removing floating IP", server, floating_ip)
 
         self.verify(20, self.compute_client.floating_ips.delete,
-                    12, "Floating IP cannot be deleted.",
+                    15, "Floating IP cannot be deleted.",
                     "floating IP deletion", floating_ip)
 
         if self.floating_ips:
             self.floating_ips.remove(floating_ip)
 
-        self.verify(40, self._delete_server, 13,
+        self.verify(40, self._delete_server, 16,
                     "Server can not be deleted. ",
                     "server deletion", server)
 
-        self.verify(40, self._remove_router, 14, "Router can not be deleted",
-                    "router deletion", router, [subnet['id']])
-        self.verify(20, self._remove_subnet, 15, "Subnet can not be deleted",
-                    "Subnet deletion", subnet)
-        self.verify(20, self._remove_network, 16,
+        self.verify(40, self._remove_router, 17, "Router can not be deleted",
+                    "router deletion", router, [subnet_v4['id']])
+        self.verify(20, self._remove_subnet, 18, "Subnet can not be deleted",
+                    "Subnet deletion", subnet_v4)
+        self.verify(20, self._remove_network, 19,
                     "Network can not be deleted", "Network deletion", network)
