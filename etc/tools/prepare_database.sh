89f46893e5fb117d53596f2eac3ad491a3bac11b
Tatyana Leontovich <tleontovich@mirantis.com>
2016-03-07 13:37:24 +0200

    Update tox.ini to skip dtabase creation steps
    after https://github.com/openstack-infra/project-config/
    commit/7334ceb347329ccbc04dc6bff536e88e12783a86 we do not need to
    create db, user, role.
    
    Also add usage of CUSTOM_OSTF_CONFIG env variable, to pass
    correctly dbpath tp fuel_plugin.config.py
    
    Closes-Bug: #1554086
    
    Change-Id: Ic2881e1cb17693ab616044029325a601d2b04e2c
    (cherry picked from commit 003272dc29ac9f917a2001d3228674685e1613a6)

--- etc/tools/prepare_database.sh
+++ etc/tools/prepare_database.sh
@@ -5,18 +5,4 @@ echo "*:*:*:${OSTF_DB_ROOT}:${OSTF_DB_ROOTPW}" > ${OSTF_DB_ROOTPGPASS}
 chmod 600 ${OSTF_DB_ROOTPGPASS}
 
 export PGPASSFILE=${OSTF_DB_ROOTPGPASS}
-
-echo "Trying to find out if role ${OSTF_DB_USER} exists"
-root_roles=$(psql -h 127.0.0.1 -U ${OSTF_DB_ROOT} -t -c "SELECT 'HERE' from pg_roles where rolname='${OSTF_DB_USER}'")
-if [[ ${root_roles} == *HERE ]];then
-  echo "Role ${OSTF_DB_USER} exists. Setting password ${OSTF_DB_PW}"
-  psql -h 127.0.0.1 -U ${OSTF_DB_ROOT} -c "ALTER ROLE ${OSTF_DB_USER} WITH SUPERUSER LOGIN PASSWORD '${OSTF_DB_PW}'"
-else
-  echo "Creating role ${OSTF_DB_USER} with password ${OSTF_DB_PASSWD}"
-  psql -h 127.0.0.1 -U ${OSTF_DB_ROOT} -c "CREATE ROLE ${OSTF_DB_USER} WITH SUPERUSER LOGIN PASSWORD '${OSTF_DB_PW}'"
-fi
-
-echo "Dropping database ${OSTF_DB} if exists"
-psql -h 127.0.0.1 -U ${OSTF_DB_ROOT} -c "DROP DATABASE IF EXISTS ${OSTF_DB}"
-echo "Creating database ${OSTF_DB}"
-psql -h 127.0.0.1 -U ${OSTF_DB_ROOT} -c "CREATE DATABASE ${OSTF_DB} OWNER ${OSTF_DB_USER}"
+cat $PGPASSFIL
