003272dc29ac9f917a2001d3228674685e1613a6
Tatyana Leontovich <tleontovich@mirantis.com>
2016-03-07 13:37:24 +0200

    Update tox.ini to skip dtabase creation steps
    after https://github.com/openstack-infra/project-config/
    commit/7334ceb347329ccbc04dc6bff536e88e12783a86 we do not need to
    create db, user, role.
    
    Also add usage of CUSTOM_OSTF_CONFIG env variable, to pass
    correctly dbpath tp fuel_plugin.config.py
    
    Closes-Bug: #1554086
    
    Change-Id: Ic2881e1cb17693ab616044029325a601d2b04e2c

--- etc/tools/prepare_settings.sh
+++ etc/tools/prepare_settings.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-cat > ${OSTF_CI_CONF} <<EOL
+cat > ${CUSTOM_OSTF_CONFIG} <<EOL
 [adapter]
 server_host = 127.0.0.1
 server_port = 8777
 nailgun_host = 127.0.0.1
 nailgun_port = 8000
+dbpath = "postgresql+psycopg2://${OSTF_DB_USER}:${OSTF_DB_PW}@localhost/${OSTF_DB}"
 log_file = ${OSTF_LOGS}"
 after_init_hook = False
 auth_enable = False
