57f5151f3af4b3cd3bfe5f705a85303f9b7dedda
Tatyana Leontovich <tleontovich@mirantis.com>
2016-03-07 13:37:24 +0200

    [WIP] Do not merge, tmp commit
    Change-Id: Ic2881e1cb17693ab616044029325a601d2b04e2c

--- etc/tools/prepare_settings.sh
+++ etc/tools/prepare_settings.sh
@@ -6,6 +6,7 @@ server_host = 127.0.0.1
 server_port = 8777
 nailgun_host = 127.0.0.1
 nailgun_port = 8000
+dbpath = "postgresql+psycopg2://${OSTF_DB_USER}:${OSTF_DB_PW}@localhost/${OSTF_DB}"
 log_file = ${OSTF_LOGS}"
 after_init_hook = False
 auth_enable = False
