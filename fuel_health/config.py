e9fda1d3080cb2d74e861b3c07a21aa8e5268e55
Volodymyr Shypyguzov <vshypyguzov@mirantis.com>
2016-04-06 17:56:37 +0300

    Add test_sriov_instance_connectivity
    +Add corresponding test
    +Add _create_port
    +Add _cleanup_ports
    +Add sriov deployment tag if computes with SR-IOV enabled interface
    are present and unit-tests for ostf-adapter
    +Add sriov_enabled physnets to cluster config
    
    Change-Id: I487c0dc0f4756b893af5d5c59ea85ba0d47faa5c
    Closes-Bug: #1566906
    Implements: blueprint test-sriov-support

--- fuel_health/config.py
+++ fuel_health/config.py
@@ -719,8 +719,8 @@ class NailgunConfig(object):
         compute_nodes = filter(lambda node: 'compute' in node['roles'],
                                data)
         online_computes = filter(
-            lambda node: 'compute' in node['roles']
-            and node['online'] is True, data)
+            lambda node: 'compute' in node['roles'] and
+                         node['online'] is True, data)
         online_computes_ips = []
         for node in online_computes:
             online_computes_ips.append(node['ip'])
@@ -730,14 +730,25 @@ class NailgunConfig(object):
         for node in compute_nodes:
             compute_ips.append(node['ip'])
         LOG.info("COMPUTES IPS %s" % compute_ips)
+        sriov_physnets = []
+        compute_ids = [node['id'] for node in online_computes]
+        for compute_id in compute_ids:
+            api_url = '/api/nodes/{}/interfaces'.format(compute_id)
+            ifaces_resp = self.req_session.get(
+                self.nailgun_url + api_url).json()
+            for iface in ifaces_resp:
+                if iface['interface_properties']['sriov']['enabled']:
+                    sriov_physnets.append(
+                        iface['interface_properties']['sriov']['physnet'])
+        self.compute.sriov_physnets = sriov_physnets
         self.compute.compute_nodes = compute_ips
         ceph_nodes = filter(lambda node: 'ceph-osd' in node['roles'],
                             data)
         self.compute.ceph_nodes = ceph_nodes
 
         online_ironic = filter(
-            lambda node: 'ironic' in node['roles']
-            and node['online'] is True, data)
+            lambda node: 'ironic' in node['roles'] and
+                         node['online'] is True, data)
         self.ironic.online_conductors = []
         for node in online_ironic:
             self.ironic.online_conductors.append(node['ip'])
