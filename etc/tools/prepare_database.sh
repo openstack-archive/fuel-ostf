24d98abd3ebd3035f4d763df1e7424c90a2b8b91
Andreas Jaeger <aj@suse.com>
2019-12-18 09:48:08 +0100

    Retire repository
    Fuel (from openstack namespace) and fuel-ccp (in x namespace)
    repositories are unused and ready to retire.
    
    This change removes all content from the repository and adds the usual
    README file to point out that the repository is retired following the
    process from
    https://docs.openstack.org/infra/manual/drivers.html#retiring-a-project
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2019-December/011647.html
    
    Depends-On: https://review.opendev.org/699362
    Change-Id: I658efd028f1b23bc21b327fc55d39a7fa5824ec1

--- etc/tools/prepare_database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Preparing pgpass file ${DB_ROOTPGPASS}"
-echo "*:*:*:${OSTF_DB_ROOT}:${OSTF_DB_ROOTPW}" > ${OSTF_DB_ROOTPGPASS}
-chmod 600 ${OSTF_DB_ROOTPGPASS}
-
-export PGPASSFILE=${OSTF_DB_ROOTPGPASS}
-cat $PGPASSFIL
